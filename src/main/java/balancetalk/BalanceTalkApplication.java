82249877f06427335f526cb3bd4a733dff1c53f4
Hanjaemo <hjm2530@gmail.com>
2024-01-26 21:43:27 +0900

    init commit

---
+++ src/main/java/balancetalk/BalanceTalkApplication.java
@@ -0,0 +1,13 @@
+package balancetalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalanceTalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalanceTalkApplication.class, args);
+	}
+
+}
