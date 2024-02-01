540bddab7cbb617aa7392882805f8be5e2b700b6
junsoo <choix712@umn.edu>
2024-02-01 21:01:11 +0900

    Revert "feat: FileType enum값 추가"
    This reverts commit f266b77bae2df56a6ef4220b5d21888e76199567.

--- src/main/java/balancetalk/domain/file/enums/FileType.java
+++ src/main/java/balancetalk/domain/file/enums/FileType.java
@@ -1,27 +1,5 @@
 package balancetalk.domain.file.enums;
 
 public enum FileType {
-    JPEG("image/jpeg", "jpg"),
-    PNG("image/png", "png"),
-    GIF("image/gif", "gif"),
-    BMP("image/bmp", "bmp"),
-    TIFF("image/tiff", "tiff"),
-    PDF("application/pdf", "pdf"),
-    TXT("text/plain", "txt");
-
-    private final String mimeType;
-    private final String fileExtension;
-
-    FileType(String mimeType, String fileExtension) {
-        this.mimeType = mimeType;
-        this.fileExtension = fileExtension;
-    }
-
-    public String getMimeType() {
-        return mimeType;
-    }
-
-    public String getFileExtension() {
-        return fileExtension;
-    }
+    JPG, JPEG, PNG
 }
