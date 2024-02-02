7ba70de92616b54cf8a76d5c77653e1b9db7d1ab
junsoo <choix712@umn.edu>
2024-02-02 16:07:08 +0900

    feat: Tag, Post, PostTag 엔티티 간 연관관계 매핑

--- src/main/java/balancetalk/domain/post/entity/Tag.java
+++ src/main/java/balancetalk/domain/post/entity/Tag.java
@@ -1,23 +1,24 @@
 package balancetalk.domain.post.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Tag {
-
     @Id
     @GeneratedValue
     @Column(name = "tag_id")
     private Long id;
 
-    // TODO 개선 필요
-//    @ManyToMany(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "post_id")
-//    private List<Post> posts = new ArrayList<>();
+    @NotNull
+    private String name;
+
+    @OneToMany(mappedBy = "tag")
+    private List<PostTag> postTags = new ArrayList<>();
 }
