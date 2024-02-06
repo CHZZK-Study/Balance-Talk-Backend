38a21d9f4823c0231cd55fb0008ba70e74b5d2f6
gywns0417 <gywns0417@gmail.com>
2024-02-06 22:20:36 +0900

    feat: 댓글 리포지토리 구현

---
+++ src/main/java/balancetalk/module/comment/domain/CommentRepository.java
@@ -0,0 +1,6 @@
+package balancetalk.module.comment.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
