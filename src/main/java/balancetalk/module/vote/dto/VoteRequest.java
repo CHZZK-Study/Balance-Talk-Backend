383c1d671a9979c6abb5b5de9eb76341e7e24128
Hanjaemo <hjm2530@gmail.com>
2024-02-07 14:44:19 +0900

    refactor: VoteRequest에 @Builder 추가

--- src/main/java/balancetalk/module/vote/dto/VoteRequest.java
+++ src/main/java/balancetalk/module/vote/dto/VoteRequest.java
@@ -4,10 +4,12 @@ import balancetalk.module.member.domain.Member;
 import balancetalk.module.post.domain.BalanceOption;
 import balancetalk.module.vote.domain.Vote;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@Builder
 public class VoteRequest {
     private Long selectedOptionId;
     private Long memberId;
