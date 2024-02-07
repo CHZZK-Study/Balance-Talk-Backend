a4cd6dffa01c56174a33c53ca5f76d4554f5dd39
Hanjaemo <hjm2530@gmail.com>
2024-02-07 14:44:40 +0900

    test: 회원 투표 수정 기능 단위 테스트

--- src/test/java/balancetalk/module/vote/application/VoteServiceTest.java
+++ src/test/java/balancetalk/module/vote/application/VoteServiceTest.java
@@ -6,10 +6,7 @@ import static org.mockito.Mockito.when;
 
 import balancetalk.module.member.domain.Member;
 import balancetalk.module.member.domain.MemberRepository;
-import balancetalk.module.post.domain.BalanceOption;
-import balancetalk.module.post.domain.BalanceOptionRepository;
-import balancetalk.module.post.domain.Post;
-import balancetalk.module.post.domain.PostRepository;
+import balancetalk.module.post.domain.*;
 import balancetalk.module.vote.domain.Vote;
 import balancetalk.module.vote.domain.VoteRepository;
 import balancetalk.module.vote.dto.VoteRequest;
@@ -17,6 +14,8 @@ import balancetalk.module.vote.dto.VotingStatusResponse;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -42,6 +41,9 @@ class VoteServiceTest {
     @Mock
     PostRepository postRepository;
 
+    @PersistenceContext
+    EntityManager em;
+
     @Test
     @DisplayName("회원과 선택지 정보를 바탕으로 투표를 생성한다.")
     void createVote_Success() {
@@ -58,8 +60,13 @@ class VoteServiceTest {
         when(memberRepository.findById(any())).thenReturn(Optional.of(member));
         when(voteRepository.save(any())).thenReturn(vote);
 
+        VoteRequest voteRequest = VoteRequest.builder()
+                .selectedOptionId(option.getId())
+                .memberId(member.getId())
+                .build();
+
         // when
-        Vote createdVote = voteService.createVote(new VoteRequest(member.getId(), option.getId()));
+        Vote createdVote = voteService.createVote(voteRequest);
 
         // then
         assertThat(createdVote.getMember()).isEqualTo(member);
@@ -67,15 +74,16 @@ class VoteServiceTest {
     }
 
     @Test
+    @DisplayName("각 선택지의 제목과 투표 수를 조회한다.")
     void readVotingStatus_Success() {
         // given
         List<Vote> votesForA = new ArrayList<>();
         for (long i = 0; i < 5; i++) {
-            votesForA.add(Vote.builder().id(i).build());
+            votesForA.add(createVote(i));
         }
         List<Vote> votesForB = new ArrayList<>();
         for (long i = 0; i < 3; i++) {
-            votesForB.add(Vote.builder().id(i).build());
+            votesForB.add(createVote(i));
         }
         List<BalanceOption> options =
                 List.of(createBalanceOption(1L, "A", votesForA), createBalanceOption(2L, "B", votesForB));
@@ -95,6 +103,64 @@ class VoteServiceTest {
         assertThat(votingStatusResponses.get(0).getVoteCount()).isEqualTo(5);
         assertThat(votingStatusResponses.get(1).getOptionTitle()).isEqualTo("B");
         assertThat(votingStatusResponses.get(1).getVoteCount()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("투표를 수정한다.")
+    void updateVote_Success() {
+        // given
+        Post post = Post.builder()
+                .id(1L)
+                .category(PostCategory.DISCUSSION)
+                .build();
+
+        BalanceOption optionA = BalanceOption.builder()
+                .id(1L)
+                .title("A")
+                .post(post)
+                .build();
+        BalanceOption optionB = BalanceOption.builder()
+                .id(2L)
+                .title("B")
+                .post(post)
+                .build();
+        Vote oldVote = Vote.builder()
+                .id(1L)
+                .balanceOption(optionA)
+                .build();
+        List<Vote> votes = List.of(oldVote);
+
+        Member member = Member.builder()
+                .id(1L)
+                .votes(votes)
+                .build();
+
+        Vote newVote = Vote.builder()
+                .id(1L)
+                .balanceOption(optionB)
+                .build();
+
+        when(postRepository.findById(post.getId())).thenReturn(Optional.of(post));
+        when(memberRepository.findById(any())).thenReturn(Optional.of(member));
+        when(balanceOptionRepository.findById(optionB.getId())).thenReturn(Optional.of(optionB));
+
+        VoteRequest voteRequest = VoteRequest.builder()
+                .selectedOptionId(optionB.getId())
+                .memberId(member.getId())
+                .build();
+
+        // when
+        Vote result = voteService.updateVote(post.getId(), voteRequest);
+
+        // then
+        assertThat(result.getBalanceOption().getId()).isEqualTo(newVote.getBalanceOption().getId());
+        assertThat(result.getBalanceOption().getTitle()).isEqualTo(newVote.getBalanceOption().getTitle());
+    }
+
+    private Vote createVote(Long id) {
+        return Vote.builder()
+                .id(id)
+                .build();
     }
 
     private BalanceOption createBalanceOption(Long id, String title, List<Vote> votes) {
