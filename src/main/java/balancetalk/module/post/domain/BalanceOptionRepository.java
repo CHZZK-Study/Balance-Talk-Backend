121cd7f58a855b5d2979799fe8e2390cab730445
gywns0417 <gywns0417@gmail.com>
2024-02-09 03:24:10 +0900

    feat: BalanceOptionRepository 임시 구현

---
+++ src/main/java/balancetalk/module/post/domain/BalanceOptionRepository.java
@@ -0,0 +1,6 @@
+package balancetalk.module.post.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BalanceOptionRepository extends JpaRepository<BalanceOption, Long> {
+}
