c95d4c6ea592635640983d284f5ee7629ac19f22
Hanjaemo <hjm2530@gmail.com>
2024-02-14 20:24:25 +0900

    feat: 게시글 추천 기능 구현

--- src/main/java/balancetalk/module/post/domain/PostLike.java
+++ src/main/java/balancetalk/module/post/domain/PostLike.java
@@ -9,9 +9,15 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PostLike {
 
