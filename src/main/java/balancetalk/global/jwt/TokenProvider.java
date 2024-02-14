126150f4dfdc5052bd74d7986652bc0224a810e5
Junsoo Choi <choix712@umn.edu>
2024-02-14 13:38:51 +0900

    feat: 토큰 생성, 검증을 하는 TokenProvider 클래스 생성

---
+++ src/main/java/balancetalk/global/jwt/TokenProvider.java
@@ -0,0 +1,67 @@
+package balancetalk.global.jwt;
+
+import io.jsonwebtoken.*;
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+
+import java.util.Base64;
+import java.util.Date;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class TokenProvider {
+
+    private String secretKey = "webfirewood";
+    private Long tokenValidTime = 30 * 60 * 1000L; // 30분 유효 시간
+    private final UserDetailsService userDetailsService;
+
+    @PostConstruct
+    protected void init() {
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes()); // Base64로 인코딩
+    }
+
+    public String createToken(String email, List<String> roles) {
+        Claims claims = Jwts.claims().setSubject(email); // JWT payload에 저장되는 정보 단위
+        claims.put("roles" , roles);
+        Date now = new Date();
+        return Jwts.builder()
+                .setClaims(claims) // 정보 저장
+                .setIssuedAt(now) // 토큰 발행 시간
+                .setExpiration(new Date(now.getTime() + tokenValidTime)) // 30분 유효시간 설정
+                .signWith(SignatureAlgorithm.HS256, secretKey) // 암호화 알고리즘과 secretKey
+                .compact();
+    }
+
+    // 인증 정보 조회
+    public Authentication getAuthentication(String token) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(this.getUserEmail(token));
+        return new UsernamePasswordAuthenticationToken(userDetails, userDetails.getAuthorities());
+    }
+
+    // 토큰에서 회원 정보 추출
+    public String getUserEmail(String token) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    // 토큰 유효성, 만료일자 확인
+    public boolean validateToken(String jwtToken) {
+        try {
+            Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwtToken);
+            return !claims.getBody().getExpiration().before(new Date()); // 만료시간 이전이면 true 반환
+        } catch (Exception e) {
+            return false; // 만료시간 이후라면 false 반환
+        }
+    }
+
+    // request Header에서 토큰 값 가져오기
+    public String resolveToken(HttpServletRequest request) {
+        return request.getHeader("X-AUTH-TOKEN");
+    }
+}
