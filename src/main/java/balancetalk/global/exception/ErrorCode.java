a19d21142ce1d029df857ee03b7ed71cba39b574
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-02-15 19:09:42 +0900

    댓글 추천 기능 구현 (#55)
    * feat: 댓글 추천 기능 구현
    
    * test: 댓글 추천 기능 단위 테스트
    
    * test: 댓글 중복 추천 시 예외 처리 단위 테스트

--- src/main/java/balancetalk/global/exception/ErrorCode.java
+++ src/main/java/balancetalk/global/exception/ErrorCode.java
@@ -28,6 +28,7 @@ public enum ErrorCode {
 
     // 409
     ALREADY_VOTE(CONFLICT, "투표는 한 번만 가능합니다."),
+    ALREADY_LIKE_COMMENT(CONFLICT, "이미 추천을 누른 댓글입니다.");
     ALREADY_LIKE_POST(CONFLICT, "이미 추천을 누른 게시글입니다.");
 
     private final HttpStatus httpStatus;
