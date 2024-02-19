c91e07a081c02608300488bf4896ef8495e8555c
Junsoo Choi <choix712@umn.edu>
2024-02-19 20:07:14 +0900

    refactor: 이메일 발송 config 수정

--- src/main/java/balancetalk/global/config/MailConfig.java
+++ src/main/java/balancetalk/global/config/MailConfig.java
@@ -1,23 +1,33 @@
 package balancetalk.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
-
 import java.util.Properties;
-
 @Configuration
 public class MailConfig {
+
+    @Value("${spring.mail.username}")
+    private String id;
+    @Value("${spring.mail.password}")
+    private String password;
+    @Value("${spring.mail.host}")
+    private String host;
+    @Value("${spring.mail.port}")
+    private int port;
 
     @Bean
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
-        javaMailSender.setHost("smtp.naver.com");
-        javaMailSender.setUsername("james4206@naver.com");
-        javaMailSender.setPassword("gozldgkwlakfk");
-        javaMailSender.setPort(465);
+        javaMailSender.setHost(host);
+        javaMailSender.setUsername(id);
+        javaMailSender.setPassword(password);
+        javaMailSender.setPort(port);
+        javaMailSender.setDefaultEncoding("UTF-8");
         javaMailSender.setJavaMailProperties(getMailProperties());
+        return javaMailSender;
     }
 
     private Properties getMailProperties() {
@@ -26,7 +36,7 @@ public class MailConfig {
         properties.setProperty("mail.smtp.auth", "true");
         properties.setProperty("mail.smtp.starttls.enable", "true");
         properties.setProperty("mail.debug", "true");
-        properties.setProperty("mail.smtp.ssl.trust","smtp.naver.com");
+        properties.setProperty("mail.smtp.ssl.trust","smtp.mailplug.co.kr"); // ssl 인증 서버 주소
         properties.setProperty("mail.smtp.ssl.enable","true");
         return properties;
     }
