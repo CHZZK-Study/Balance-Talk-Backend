2f795b875045bc70d577b1cf34303705a4bf757a
Junsoo Choi <choix712@umn.edu>
2024-02-19 15:59:02 +0900

    refactor: mail config 설정

---
+++ src/main/java/balancetalk/global/config/MailConfig.java
@@ -0,0 +1,33 @@
+package balancetalk.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+        javaMailSender.setHost("smtp.naver.com");
+        javaMailSender.setUsername("james4206@naver.com");
+        javaMailSender.setPassword("gozldgkwlakfk");
+        javaMailSender.setPort(465);
+        javaMailSender.setJavaMailProperties(getMailProperties());
+    }
+
+    private Properties getMailProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("mail.transport.protocol", "smtp");
+        properties.setProperty("mail.smtp.auth", "true");
+        properties.setProperty("mail.smtp.starttls.enable", "true");
+        properties.setProperty("mail.debug", "true");
+        properties.setProperty("mail.smtp.ssl.trust","smtp.naver.com");
+        properties.setProperty("mail.smtp.ssl.enable","true");
+        return properties;
+    }
+}
