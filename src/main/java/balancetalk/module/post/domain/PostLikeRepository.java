49445684a32e4c27f66fab6832d112dc956acb61
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-02-20 00:33:40 +0900

    feat: 게시글 추천 취소 기능 구현 (#78)

--- src/main/java/balancetalk/module/post/domain/PostLikeRepository.java
+++ src/main/java/balancetalk/module/post/domain/PostLikeRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface PostLikeRepository extends JpaRepository<PostLike, Long> {
 
     boolean existsByMemberAndPost(Member member, Post post);
+
+    void deleteByMemberAndPost(Member member, Post post);
 }
