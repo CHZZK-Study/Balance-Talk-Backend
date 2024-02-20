9bc01dfc72523a97cb4f4ca1f67101fe43fe0c38
Hanjaemo <hjm2530@gmail.com>
2024-02-21 03:05:59 +0900

    chore: CORS 설정

---
+++ src/main/java/balancetalk/global/config/CorsConfig.java
@@ -0,0 +1,21 @@
+package balancetalk.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    private final int MAX_AGE_SEC = 3600;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                .allowCredentials(true)
+                .maxAge(MAX_AGE_SEC);
+    }
+}
