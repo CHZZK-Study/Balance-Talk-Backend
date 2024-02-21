f0b61b3ddd83da80d054ce5fb3e85b69d4c68eb0
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2024-02-22 00:16:57 +0900

    Redis 설정 (#104)
    * feat: Redis 디펜던시 추가
    
    * feat: yml 환경변수 세팅
    
    * refactor: yml 수정
    
    * feat: config 파일 추가
    
    * feat: service 클래스 추가
    
    * fix: BalanceOption import 추가
    
    * fix: Redis CRUD 단위 테스트 추가

---
+++ src/main/java/balancetalk/global/config/RedisConfig.java
@@ -0,0 +1,35 @@
+package balancetalk.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@EnableRedisRepositories
+public class RedisConfig {
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        return redisTemplate;
+    }
+}
