4ec22d874b7a0d8590c8a5870fd324e32a9bf3a1
Junsoo Choi <choix712@umn.edu>
2024-02-22 23:58:08 +0900

    refactor: JwtFilter 에러 케이스 추가

--- src/main/java/balancetalk/global/jwt/JwtAuthenticationFilter.java
+++ src/main/java/balancetalk/global/jwt/JwtAuthenticationFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.RedisConnectionFailureException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
@@ -21,14 +22,17 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
 
-        // 토큰이 유효할 때
-        if (token != null && jwtTokenProvider.validateToken(token)) {
-            // 토큰으로부터 유저 정보를 받는다
-            Authentication authentication = jwtTokenProvider.getAuthentication(token);
-            // SecurityContext에 객체 저장
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+        try {
+            if (token != null && jwtTokenProvider.validateToken(token)) {
+                Authentication auth = jwtTokenProvider.getAuthentication(token);
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        } catch (RedisConnectionFailureException e) {
+            SecurityContextHolder.clearContext();
+            throw new IllegalAccessError("Redis 연결 실패");
+        } catch (Exception e) {
+            throw new IllegalArgumentException("JWT가 유효하지 않음.");
         }
-        // 다음 필터로 진행
         chain.doFilter(request, response);
     }
 }
