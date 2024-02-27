9485e1c1710282d1f154d9da0c8f54e66acfdcef
Hanjaemo <hjm2530@gmail.com>
2024-02-27 21:46:31 +0900

    chore: 기존에 작성된 FileService 클래스 제거

--- src/main/java/balancetalk/module/file/presentation/FileController.java
+++ src/main/java/balancetalk/module/file/presentation/FileController.java
@@ -1,6 +1,5 @@
 package balancetalk.module.file.presentation;
 
-import balancetalk.module.file.application.FileService;
 import balancetalk.module.file.application.S3UploadService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -11,19 +10,12 @@ import org.springframework.web.multipart.MultipartFile;
 @RequiredArgsConstructor
 @RequestMapping("/files")
 public class FileController {
-    private final FileService fileService;
+
     private final S3UploadService s3UploadService;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/image/upload")
     public String uploadImage(@RequestParam("file") MultipartFile file) {
         return s3UploadService.uploadImage(file);
-    }
-
-    @ResponseStatus(HttpStatus.OK)
-    @GetMapping("/download/{fileId}")
-    public String downloadFile(@PathVariable Long fileId) {
-        fileService.downloadFile(fileId);
-        return "파일이 다운로드되었습니다.";
     }
 }
