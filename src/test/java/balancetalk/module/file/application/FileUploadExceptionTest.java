c3b1fddfc88b8187fbdf3ebc64dd73b358c88cfa
gywns0417 <gywns0417@gmail.com>
2024-02-29 01:36:05 +0900

    test: 지원하지 않는 파일 형식 예외 테스트 추가

--- src/test/java/balancetalk/module/file/application/FileUploadExceptionTest.java
+++ src/test/java/balancetalk/module/file/application/FileUploadExceptionTest.java
@@ -1,6 +1,7 @@
 package balancetalk.module.file.application;
 
 import balancetalk.module.authmail.application.MailService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -57,6 +58,7 @@ public class FileUploadExceptionTest {
     }
 
     @Test
+    @DisplayName("파일 업로드 실패 - 파일 이름이 너무 김")
     public void uploadFileWithVeryLongName_ShouldReturnBadRequest() {
         Resource veryLongNameFile = new ClassPathResource("veryVeryVeryVeryVeryVeryLongFileNameExceedingTheLimit.jpg");
         HttpHeaders headers = new HttpHeaders();
@@ -65,6 +67,29 @@ public class FileUploadExceptionTest {
 
         MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
         body.add("file", veryLongNameFile);
+
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(
+                "http://localhost:" + port + "/files/image/upload",
+                HttpMethod.POST,
+                requestEntity,
+                String.class
+        );
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+
+    @Test
+    @DisplayName("파일 업로드 실패 - 지원하지 않는 파일 형식")
+    public void uploadUnsupportedFileType_ShouldReturnUnsupportedMediaType() throws Exception {
+        Resource unsupportedFile = new ClassPathResource("unsupportedFileType.etc");
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        body.add("file", unsupportedFile);
 
         HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
 
