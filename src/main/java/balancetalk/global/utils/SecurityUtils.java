50c4c426d3c3874ecf125804d075c3cbe0d2e743
gywns0417 <gywns0417@gmail.com>
2024-03-05 20:50:36 +0900

    feat: SecurityUtils 클래스 기본 생성자 접근 방지 구현

--- src/main/java/balancetalk/global/utils/SecurityUtils.java
+++ src/main/java/balancetalk/global/utils/SecurityUtils.java
@@ -3,11 +3,14 @@ package balancetalk.global.utils;
 import balancetalk.global.exception.BalanceTalkException;
 import balancetalk.module.member.domain.Member;
 import balancetalk.module.member.domain.MemberRepository;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import static balancetalk.global.exception.ErrorCode.NOT_FOUND_MEMBER;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class SecurityUtils {
     public static Member getCurrentMember(MemberRepository memberRepository) { // TODO: global static 메서드로 전환?
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
