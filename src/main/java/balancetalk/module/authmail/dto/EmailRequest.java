678b14e230ce9bca506c116b4e67e5a064296cc5
Junsoo Choi <choix712@umn.edu>
2024-03-05 22:36:18 +0900

    refactor: 메일 dto에 기본 생성자 추가

--- src/main/java/balancetalk/module/authmail/dto/EmailRequest.java
+++ src/main/java/balancetalk/module/authmail/dto/EmailRequest.java
@@ -4,9 +4,11 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class EmailRequest {
 
     @Email
