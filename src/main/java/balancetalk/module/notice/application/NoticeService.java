79226cc47e5f02f4a381747ea332071e2cdb36d7
gywns0417 <gywns0417@gmail.com>
2024-03-08 17:22:34 +0900

    style: optimze imports 적용

--- src/main/java/balancetalk/module/notice/application/NoticeService.java
+++ src/main/java/balancetalk/module/notice/application/NoticeService.java
@@ -11,7 +11,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
--- src/main/java/balancetalk/module/notice/domain/Notice.java
+++ src/main/java/balancetalk/module/notice/domain/Notice.java
@@ -7,6 +7,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
