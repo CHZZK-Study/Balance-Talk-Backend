a567c22018434456082aeea4160f46ded6085500
gywns0417 <gywns0417@gmail.com>
2024-03-08 14:31:15 +0900

    feat: NoticeRepository 클래스 작성

---
+++ src/main/java/balancetalk/module/notice/domain/NoticeRepository.java
@@ -0,0 +1,6 @@
+package balancetalk.module.notice.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NoticeRepository extends JpaRepository<Notice, Long> {
+}
