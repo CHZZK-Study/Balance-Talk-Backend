11c2a1575693ba9ff8062a6d6540c69c367696a5
gywns0417 <gywns0417@gmail.com>
2024-03-17 01:49:37 +0900

    test: 시큐리티 인증 기능 추가로 인한 기존 공지 테스트 코드 수정

--- src/test/java/balancetalk/module/notice/presentation/NoticeControllerTest.java
+++ src/test/java/balancetalk/module/notice/presentation/NoticeControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -21,6 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@WithMockUser
 class NoticeControllerTest {
 
     @Autowired
