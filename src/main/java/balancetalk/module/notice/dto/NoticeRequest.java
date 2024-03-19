f6b1c1d8d6305b05ec627c4c4e6fbb76896e7a6f
king_0417 <73704053+gywns0417@users.noreply.github.com>
2024-03-19 21:37:28 +0900

    공지사항 수정, 삭제 기능, 파일 첨부 구현 완료 (#186)
    * feat: 공지사항 수정, 삭제 에러 코드 추가 및 403번 에러로 변경
    
    * feat: 공지사항 수정, 삭제 기능 구현
    
    * test: 공지사항 수정, 삭제 기능에 대한 테스트 코드 작성 및 통과 완료
    
    * feat: 공지사항 파일 첨부 구현
    
    * fix: 공지사항 파일 첨부 시 Response에서 파일 목록이 null로 뜨던 에러 수정
    
    * test: 공지사항 기능 추가 구현에 대한 테스트 코드 수정 및 추가 작성 완료
    
    * test: 테스트 주석 처리

--- src/main/java/balancetalk/module/notice/dto/NoticeRequest.java
+++ src/main/java/balancetalk/module/notice/dto/NoticeRequest.java
@@ -1,5 +1,6 @@
 package balancetalk.module.notice.dto;
 
+import balancetalk.module.file.domain.File;
 import balancetalk.module.member.domain.Member;
 import balancetalk.module.notice.domain.Notice;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -8,6 +9,9 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.lang.Nullable;
+
+import java.util.List;
 
 @Data
 @Builder
@@ -24,11 +28,14 @@ public class NoticeRequest {
     @Schema(description = "공지사항 내용", example = "공지사항 내용")
     private String content;
 
-    public Notice toEntity(Member member) {
+    @Schema(description = "파일 리스트", example = "[\"4df23447-2355-45h2-8783-7f6gd2ceb848_고양이.jpg\"," +
+            " \"4df23447-2355-45h2-8783-7f6gd2ceb848_강아지.jpg\"]")
+    private List<String> storedFileNames;
 
+    public Notice toEntity(Member member) {
         return Notice.builder()
-                .title(title)
-                .content(content)
+                .title(this.title)
+                .content(this.content)
                 .member(member)
                 .build();
     }
