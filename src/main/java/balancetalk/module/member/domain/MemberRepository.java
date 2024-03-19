a8dcc4d39d4ed62df1601e89346d94d07b641bb5
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2024-03-19 19:49:36 +0900

    비밀번호 찾기 기능 구현 (#205)
    * feat: 임시 비밀번호 발급 기능 구현
    
    * feat: 임시 비밀번호 발급 후 데이터베이스에 저장된 비밀번호 변경
    
    * feat: 이메일 발송이 실패한 경우 에러 코드 추가
    
    * test: 임시 비밀번호 발송 후 회원 비밀번호 단위 테스트 추가
    
    * refactor: 리뷰 받은 내용들 반영하여 수정

--- src/main/java/balancetalk/module/member/domain/MemberRepository.java
+++ src/main/java/balancetalk/module/member/domain/MemberRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String username);
     boolean existsByNickname(String nickname);
-    boolean existsByEmail(String email);
+
     void deleteByEmail(String email);
 
     @Query("select m.id from Member m JOIN m.votes v WHERE v.balanceOption.id = :balanceOptionId")
