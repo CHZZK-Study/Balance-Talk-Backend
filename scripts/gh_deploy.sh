f523ae18aa9bdfe9a84d6268eddc0dd34d33bf6c
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-03-22 21:08:02 +0900

    Update gh_deploy.sh

--- scripts/gh_deploy.sh
+++ scripts/gh_deploy.sh
@@ -14,17 +14,8 @@ echo "> build 파일명: $JAR_NAME" >> $DEPLOY_LOG_PATH
 echo "> build 파일 복사" >> $DEPLOY_LOG_PATH
 cp $BUILD_JAR $BUILD_PATH
 
-echo "> 현재 동작중인 애플리케이션 PID 체크" >> $DEPLOY_LOG_PATH
-CURRENT_PID=$(pgrep -f $JAR_NAME)
-
-if [ -z $CURRENT_PID ]
-then
-  echo "> 현재 동작중인 애플리케이션이 존재하지 않음" >> $DEPLOY_LOG_PATH
-else
-  echo "> 현재 동작중인 애플리케이션이 존재하여 강제 종료 진행" >> $DEPLOY_LOG_PATH
-  echo "> kill -9 $CURRENT_PID" >> $DEPLOY_LOG_PATH
-  kill -9 $CURRENT_PID
-fi
+echo "> 애플리케이션 재구동" >> $DEPLOY_LOG_PATH
+pkill -9 -ef $JAR_NAME
 
 DEPLOY_JAR=$BUILD_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포" >> $DEPLOY_LOG_PATH
