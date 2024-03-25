fad99498d661472a66a9db3dd1554bb5fbb59c41
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-03-25 17:19:36 +0900

    Update CorsConfig.java

--- src/main/java/balancetalk/global/config/CorsConfig.java
+++ src/main/java/balancetalk/global/config/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://balancetalk.kro.kr")
                 .allowedHeaders("*")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
                 .maxAge(MAX_AGE_SEC);
