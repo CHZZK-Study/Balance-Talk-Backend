985aec985ab3cf6620f824e8974d4a393ba83bb8
gywns0417 <gywns0417@gmail.com>
2024-03-25 18:47:28 +0900

    fix: VoteRequest에 누락된 @NoArgsConstructor 어노테이션 추가

--- src/main/java/balancetalk/module/vote/dto/VoteRequest.java
+++ src/main/java/balancetalk/module/vote/dto/VoteRequest.java
@@ -7,8 +7,10 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class VoteRequest {
