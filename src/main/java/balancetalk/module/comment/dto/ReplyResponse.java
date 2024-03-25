a8d8cd98e3fc177ede1216f59f2be87dfe159985
gywns0417 <gywns0417@gmail.com>
2024-03-25 19:40:44 +0900

    style: 스웨거 스키마 (댓글 -> 답글) 수정

--- src/main/java/balancetalk/module/comment/dto/ReplyResponse.java
+++ src/main/java/balancetalk/module/comment/dto/ReplyResponse.java
@@ -15,37 +15,37 @@ import java.util.Optional;
 @AllArgsConstructor
 @Builder
 public class ReplyResponse {
-    @Schema(description = "댓글 id", example = "1345")
+    @Schema(description = "답글 id", example = "1345")
     private Long id;
 
     @Schema(description = "부모 댓글 id", example = "2")
     private Long parentCommentId;
 
-    @Schema(description = "댓글 내용", example = "댓글 내용...")
+    @Schema(description = "답글 내용", example = "댓글 내용...")
     private String content;
 
-    @Schema(description = "댓글 작성자", example = "닉네임")
+    @Schema(description = "답글 작성자", example = "닉네임")
     private String memberName;
 
-    @Schema(description = "해당 댓글에 맞는 게시글 id", example = "1")
+    @Schema(description = "해당 답글에 맞는 게시글 id", example = "1")
     private Long postId;
 
-    @Schema(description = "해당 댓글에 맞는 선택지 id", example = "23")
+    @Schema(description = "해당 답글에 맞는 선택지 id", example = "23")
     private Long selectedOptionId;
 
-    @Schema(description = "댓글 추천 수", example = "24")
+    @Schema(description = "답글 추천 수", example = "24")
     private int likesCount;
 
     @Schema(description = "추천 여부", example = "true")
     private boolean myLike;
 
-    @Schema(description = "댓글 생성 날짜")
+    @Schema(description = "답글 생성 날짜")
     private LocalDateTime createdAt;
 
-    @Schema(description = "댓글 수정 날짜")
+    @Schema(description = "답글 수정 날짜")
     private LocalDateTime lastModifiedAt;
 
-    @Schema(description = "댓글 작성자 프로필 사진 경로", example = "https://balance-talk-static-files4df23447-2355-45h2-8783-7f6gd2ceb848_프로필.jpg")
+    @Schema(description = "답글 작성자 프로필 사진 경로", example = "https://balance-talk-static-files4df23447-2355-45h2-8783-7f6gd2ceb848_프로필.jpg")
     private String profileImageUrl;
 
     public static ReplyResponse fromEntity(Comment comment, Long balanceOptionId, boolean myLike) {
