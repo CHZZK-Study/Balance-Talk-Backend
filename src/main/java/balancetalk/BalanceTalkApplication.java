6fc5a00555e9146ae937d80c966c68e213c16f06
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-03-26 02:19:23 +0900

    Update BalanceTalkApplication.java

--- src/main/java/balancetalk/BalanceTalkApplication.java
+++ src/main/java/balancetalk/BalanceTalkApplication.java
@@ -1,5 +1,7 @@
 package balancetalk;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
