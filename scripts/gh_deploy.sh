cb78781ee257f71b4b5b5404cc04edb084ecb29f
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-03-28 01:15:56 +0900

    Update gh_deploy.sh

--- scripts/gh_deploy.sh
+++ scripts/gh_deploy.sh
@@ -16,7 +16,7 @@ cp $BUILD_JAR $BUILD_PATH
 
 echo "> 애플리케이션 재구동" >> $DEPLOY_LOG_PATH
 
-CURRENT_PID=$(lsof -i tcp:8080)
+CURRENT_PID=$(pgrep -f $JAR_NAME)
 echo “현재 구동중인 애플리케이션 PID: $CURRENT_PID”
 if [ -z “$CURRENT_PID” ]; then
     echo “> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다.”
