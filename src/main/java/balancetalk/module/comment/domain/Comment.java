12be48252d20545bffbc1b3fd0ce91a6e9a99ca7
gywns0417 <gywns0417@gmail.com>
2024-04-04 16:06:16 +0900

    feat: Comment 엔티티에 답글 수 필드 추가

--- src/main/java/balancetalk/module/comment/domain/Comment.java
+++ src/main/java/balancetalk/module/comment/domain/Comment.java
@@ -46,6 +46,9 @@ public class Comment extends BaseTimeEntity {
     @JoinColumn(name = "parent_id")
     private Comment parent;
 
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
+    private List<Comment> replies = new ArrayList<>();
+
     @OneToMany(mappedBy = "comment", cascade = CascadeType.ALL)
     private List<CommentLike> likes = new ArrayList<>();
 
