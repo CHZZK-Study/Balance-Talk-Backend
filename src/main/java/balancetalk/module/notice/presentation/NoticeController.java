e9442f62b6a9ae5fe305b8e169204d797615445c
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2024-06-20 14:57:34 +0900

    chore: 톡픽, 밸런스 게임 Swagger 문서 작성 및 회원 조회 응답값 수정 (#327)

--- src/main/java/balancetalk/module/notice/presentation/NoticeController.java
+++ src/main/java/balancetalk/module/notice/presentation/NoticeController.java
@@ -23,50 +23,50 @@ import static balancetalk.global.exception.ErrorCode.PAGE_SIZE_ZERO;
 @Tag(name = "notice", description = "공지사항 API")
 public class NoticeController {
 
-    private final NoticeService noticeService;
-
-    @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping
-    @Operation(summary = "공지사항 생성" , description = "로그인 상태인 관리자가 게시글을 작성한다.")
-    public NoticeResponse createNotice(@Valid @RequestBody final NoticeRequest noticeRequest) {
-        return noticeService.createNotice(noticeRequest);
-    }
-
-    @ResponseStatus(HttpStatus.OK)
-    @GetMapping
-    @Operation(summary = "전체 공지사항 조회" , description = "작성된 모든 공지사항을 페이지네이션을 이용해 조회한다.")
-    public Page<NoticeResponse> findAllNotices(@RequestParam(value = "page", defaultValue = "0") int page,
-                                               @RequestParam(required = false, value = "size", defaultValue = "13") int size) {
-
-        if (page < 0) {
-            throw new BalanceTalkException(PAGE_NUMBER_ZERO);
-        }
-        if (size <= 0) {
-            throw new BalanceTalkException(PAGE_SIZE_ZERO);
-        }
-
-        Pageable pageable = PageRequest.of(page, size);
-        return noticeService.findAllNotices(pageable);
-    }
-
-    @ResponseStatus(HttpStatus.OK)
-    @GetMapping("/{noticeId}")
-    @Operation(summary = "특정 공지사항 조회" , description = "특정 공지사항을 조회한다.")
-    public NoticeResponse findNoticeById(@PathVariable Long noticeId) {
-        return noticeService.findNoticeById(noticeId);
-    }
-
-    @PutMapping("/{noticeId}")
-    @ResponseStatus(HttpStatus.OK)
-    @Operation(summary = "공지사항 수정", description = "기존 공지사항을 수정한다.")
-    public NoticeResponse updateNotice(@PathVariable Long noticeId, @Valid @RequestBody NoticeRequest noticeRequest) {
-        return noticeService.updateNotice(noticeId, noticeRequest);
-    }
-
-    @DeleteMapping("/{noticeId}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    @Operation(summary = "공지사항 삭제", description = "특정 공지사항을 삭제한다.")
-    public void deleteNotice(@PathVariable Long noticeId) {
-        noticeService.deleteNotice(noticeId);
-    }
+//    private final NoticeService noticeService;
+//
+//    @ResponseStatus(HttpStatus.CREATED)
+//    @PostMapping
+//    @Operation(summary = "공지사항 생성" , description = "로그인 상태인 관리자가 게시글을 작성한다.")
+//    public NoticeResponse createNotice(@Valid @RequestBody final NoticeRequest noticeRequest) {
+//        return noticeService.createNotice(noticeRequest);
+//    }
+//
+//    @ResponseStatus(HttpStatus.OK)
+//    @GetMapping
+//    @Operation(summary = "전체 공지사항 조회" , description = "작성된 모든 공지사항을 페이지네이션을 이용해 조회한다.")
+//    public Page<NoticeResponse> findAllNotices(@RequestParam(value = "page", defaultValue = "0") int page,
+//                                               @RequestParam(required = false, value = "size", defaultValue = "13") int size) {
+//
+//        if (page < 0) {
+//            throw new BalanceTalkException(PAGE_NUMBER_ZERO);
+//        }
+//        if (size <= 0) {
+//            throw new BalanceTalkException(PAGE_SIZE_ZERO);
+//        }
+//
+//        Pageable pageable = PageRequest.of(page, size);
+//        return noticeService.findAllNotices(pageable);
+//    }
+//
+//    @ResponseStatus(HttpStatus.OK)
+//    @GetMapping("/{noticeId}")
+//    @Operation(summary = "특정 공지사항 조회" , description = "특정 공지사항을 조회한다.")
+//    public NoticeResponse findNoticeById(@PathVariable Long noticeId) {
+//        return noticeService.findNoticeById(noticeId);
+//    }
+//
+//    @PutMapping("/{noticeId}")
+//    @ResponseStatus(HttpStatus.OK)
+//    @Operation(summary = "공지사항 수정", description = "기존 공지사항을 수정한다.")
+//    public NoticeResponse updateNotice(@PathVariable Long noticeId, @Valid @RequestBody NoticeRequest noticeRequest) {
+//        return noticeService.updateNotice(noticeId, noticeRequest);
+//    }
+//
+//    @DeleteMapping("/{noticeId}")
+//    @ResponseStatus(HttpStatus.NO_CONTENT)
+//    @Operation(summary = "공지사항 삭제", description = "특정 공지사항을 삭제한다.")
+//    public void deleteNotice(@PathVariable Long noticeId) {
+//        noticeService.deleteNotice(noticeId);
+//    }
 }
---
+++ src/main/java/balancetalk/module/post/presentation/TalkController.java
@@ -0,0 +1,62 @@
+package balancetalk.module.post.presentation;
+
+import balancetalk.module.post.dto.GameRequest;
+import balancetalk.module.post.dto.GameResponse;
+import balancetalk.module.post.dto.TalkRequest;
+import balancetalk.module.post.dto.TalkResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("talks")
+@Tag(name = "talk", description = "오늘의 톡픽 API")
+public class TalkController {
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    @Operation(summary = "오늘의 톡픽 생성")
+    public TalkResponse createTalkPick(@RequestBody final TalkRequest request) {
+        return TalkResponse.builder().build();
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/{postId}/likes")
+    @Operation(summary = "오늘의 톡픽 추천", description = "post-id에 해당하는 게시글에 추천을 누른다.")
+    public String likePost(@PathVariable Long postId) {
+        return "요청이 정상적으로 처리되었습니다.";
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{postId}/likes")
+    @Operation(summary = "오늘의 톡픽 추천 취소", description = "post-id에 해당하는 게시글에 누른 추천을 취소한다.")
+    public String cancelLikePost(@PathVariable Long postId) {
+        return "요청이 정상적으로 처리되었습니다.";
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/{postId}")
+    @Operation(summary = "오늘의 톡픽 조회", description = "오늘의 톡픽 단건 조회")
+    public TalkResponse findPost(@PathVariable Long postId) {
+        return TalkResponse.builder().build();
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @PatchMapping("/{postId}")
+    @Operation(summary = "오늘의 톡픽 수정", description = "오늘의 톡픽 게시글을 수정한다")
+    public GameResponse updatePost(@PathVariable Long postId, @RequestBody final GameRequest request) {
+        return GameResponse.builder().build();
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @DeleteMapping("/{postId}")
+    @Operation(summary = "오늘의 톡픽 삭제", description = "오늘의 톡픽 게시글을 삭제한다")
+    public String updatePost(@PathVariable Long postId) {
+        return "게시글이 삭제되었습니다.";
+    }
+}
