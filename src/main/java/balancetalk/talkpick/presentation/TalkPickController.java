8270573c13188bfbaf4349e8fec4810247b77512
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-06-30 01:55:14 +0900

    chore: 톡픽 상세 조회 API 명세 추가

--- src/main/java/balancetalk/talkpick/presentation/TalkPickController.java
+++ src/main/java/balancetalk/talkpick/presentation/TalkPickController.java
@@ -1,13 +1,17 @@
 package balancetalk.talkpick.presentation;
 
 import balancetalk.global.common.ApiResponse;
+import balancetalk.talkpick.dto.TalkPickDetailResponse;
 import balancetalk.talkpick.dto.TalkPickRequest;
 import balancetalk.talkpick.dto.TalkPickResponse;
 import balancetalk.talkpick.dto.TodayTalkPickResponse;
+import balancetalk.vote.domain.VoteOption;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
+
+import static balancetalk.vote.domain.VoteOption.*;
 
 @RestController
 @RequestMapping("/talks")
@@ -32,6 +36,12 @@ public class TalkPickController {
                 0L,
                 request.getOptionA(),
                 request.getOptionB()));
+    }
+
+    @Operation(summary = "톡픽 상세 조회", description = "톡픽 상세 페이지를 조회합니다.")
+    @GetMapping("/{talkPickId}")
+    public ApiResponse<TalkPickDetailResponse> findTalkPickDetail(@PathVariable final Long talkPickId) {
+        return ApiResponse.ok(new TalkPickDetailResponse(1L, "제목", "내용", "요약", "O", "X", 142L, 12L, true, false, true, A));
     }
 
     @Operation(summary = "톡픽 수정", description = "톡픽을 수정합니다.")
