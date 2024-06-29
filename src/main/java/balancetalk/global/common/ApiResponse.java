3d587d46291161416f672b949b97c6785164de0c
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-06-30 01:15:38 +0900

    chore: 공통 응답 포맷 스웨거 설정 추가

--- src/main/java/balancetalk/global/common/ApiResponse.java
+++ src/main/java/balancetalk/global/common/ApiResponse.java
@@ -1,17 +1,25 @@
 package balancetalk.global.common;
 
-import balancetalk.global.exception.ErrorCode;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 import static org.springframework.http.HttpStatus.OK;
 
+@Getter
 public class ApiResponse<T> {
 
     private static final String SUCCESS = "SUCCESS";
 
+    @Schema(description = "HTTP 상태 코드", example = "201")
     private int code;
+
+    @Schema(description = "HTTP 상태", example = "CREATED")
     private HttpStatus status;
+
+    @Schema(description = "HTTP 상태 메시지", example = "HTTP 상태 메시지")
     private String message;
+
     private T data;
 
     private ApiResponse(HttpStatus status, String message, T data) {
