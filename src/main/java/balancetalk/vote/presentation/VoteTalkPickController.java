dcb5b5c32c39bcb4238c561ecd91029dadbe4c92
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-06-30 02:12:21 +0900

    chore: 투표 결과 조회 API 명세 추가

--- src/main/java/balancetalk/vote/presentation/VoteTalkPickController.java
+++ src/main/java/balancetalk/vote/presentation/VoteTalkPickController.java
@@ -2,6 +2,7 @@ package balancetalk.vote.presentation;
 
 import balancetalk.global.common.ApiResponse;
 import balancetalk.vote.dto.VoteRequest;
+import balancetalk.vote.dto.VoteResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
@@ -11,10 +12,16 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name = "vote", description = "투표 API")
 public class VoteTalkPickController {
 
-    @Operation(summary = "톡픽 투표", description = "톡픽에서 원하는 선택지에 투표합니다.")
+    @Operation(summary = "톡픽 투표 생성", description = "톡픽에서 원하는 선택지에 투표합니다.")
     @PostMapping
     public ApiResponse<Object> voteTalkPick(@PathVariable Long talkPickId,
                                             @RequestBody VoteRequest request) {
         return ApiResponse.ok();
+    }
+
+    @Operation(summary = "톡픽 투표 결과 조회", description = "톡픽 투표 결과를 조회합니다.")
+    @GetMapping
+    public ApiResponse<VoteResponse> getVoteResultTalkPick(@PathVariable Long talkPickId) {
+        return ApiResponse.ok(new VoteResponse(23, 12));
     }
 }
