e035392f9719df45fd36d34ff6ba6f0bac7a7f90
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-06-30 02:02:42 +0900

    refactor: 오늘의 톡픽 관련 API 컨트롤러 분리

--- src/main/java/balancetalk/talkpick/presentation/TalkPickController.java
+++ src/main/java/balancetalk/talkpick/presentation/TalkPickController.java
@@ -4,25 +4,16 @@ import balancetalk.global.common.ApiResponse;
 import balancetalk.talkpick.dto.TalkPickDetailResponse;
 import balancetalk.talkpick.dto.TalkPickRequest;
 import balancetalk.talkpick.dto.TalkPickResponse;
-import balancetalk.talkpick.dto.TodayTalkPickResponse;
-import balancetalk.vote.domain.VoteOption;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
-import static balancetalk.vote.domain.VoteOption.*;
+import static balancetalk.vote.domain.VoteOption.A;
 
 @RestController
 @RequestMapping("/talks")
 @Tag(name = "talk_pick", description = "톡픽 API")
 public class TalkPickController {
-
-    @Operation(summary = "오늘의 톡픽 조회 (메인)", description = "메인 페이지에서 오늘의 톡픽을 조회합니다.")
-    @GetMapping("/today")
-    public ApiResponse<TodayTalkPickResponse> findTodayTalkPick(final Pageable pageable) {
-        return ApiResponse.ok(new TodayTalkPickResponse(1L, "제목", "요약", "O", "X"));
-    }
 
     @Operation(summary = "톡픽 생성", description = "톡픽을 생성합니다.")
     @PostMapping
