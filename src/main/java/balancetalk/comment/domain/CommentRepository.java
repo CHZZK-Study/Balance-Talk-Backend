3d89ad289d335450459fe0aabd78492986d66721
gywns0417 <gywns0417@gmail.com>
2024-06-30 02:48:16 +0900

    feat: CommentRepository 페이지네이션 추가

--- src/main/java/balancetalk/comment/domain/CommentRepository.java
+++ src/main/java/balancetalk/comment/domain/CommentRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findByTalkPickId(Long talkPickId);
+    Page<Comment> findAllByTalkPickId(Long talkPickId, Pageable pageable);
 
     Page<Comment> findAllByMemberEmail(String email, Pageable pageable);
 
