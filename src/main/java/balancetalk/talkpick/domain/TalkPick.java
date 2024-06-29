c20f9dd21610d8e4f642e573ecf2ca39265b9f91
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-06-29 22:14:40 +0900

    feat: 톡픽 관련 엔티티 클래스 작성

---
+++ src/main/java/balancetalk/talkpick/domain/TalkPick.java
@@ -0,0 +1,61 @@
+package balancetalk.talkpick.domain;
+
+import balancetalk.global.common.BaseTimeEntity;
+import balancetalk.module.member.domain.Member;
+import balancetalk.vote.domain.Vote;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class TalkPick extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @NotBlank
+    @Size(max = 255)
+    private String title;
+
+    @NotBlank
+    @Size(max = 255)
+    private String summary;
+
+    @NotBlank
+    @Size(max = 255)
+    private String content;
+
+    @NotBlank
+    @Size(max = 50)
+    @Column(name = "option_a")
+    private String optionA;
+
+    @NotBlank
+    @Size(max = 50)
+    @Column(name = "option_b")
+    private String optionB;
+
+    @PositiveOrZero
+    private Long views = 0L;
+
+    @Enumerated(value = EnumType.STRING)
+    private ViewStatus viewStatus = ViewStatus.NORMAL;
+
+    @OneToMany(mappedBy = "talkPick")
+    private List<Vote> votes = new ArrayList<>();
+
+    @OneToMany(mappedBy = "talkPick")
+    private List<TalkPickFile> talkPickFiles = new ArrayList<>();
+}
---
+++ src/main/java/balancetalk/talkpick/domain/TalkPickLike.java
@@ -0,0 +1,26 @@
+package balancetalk.talkpick.domain;
+
+import balancetalk.global.common.BaseTimeEntity;
+import balancetalk.module.member.domain.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class TalkPickLike extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "talk_pick_id")
+    private TalkPick talkPick;
+
+    private Boolean active = true;
+}
