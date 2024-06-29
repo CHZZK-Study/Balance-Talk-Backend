07bc55e405833b0110fd070b823a4985691bb7f2
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-06-30 01:28:25 +0900

    chore: 오늘의 톡픽 API 명세 추가

--- src/main/java/balancetalk/talkpick/presentation/TalkPickController.java
+++ src/main/java/balancetalk/talkpick/presentation/TalkPickController.java
@@ -3,12 +3,11 @@ package balancetalk.talkpick.presentation;
 import balancetalk.global.common.ApiResponse;
 import balancetalk.talkpick.dto.CreateTalkPickRequest;
 import balancetalk.talkpick.dto.CreateTalkPickResponse;
+import balancetalk.talkpick.dto.TodayTalkPickResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/talks")
@@ -27,5 +26,11 @@ public class TalkPickController {
                 0L,
                 request.getOptionA(),
                 request.getOptionB()));
+    }
+
+    @Operation(summary = "오늘의 톡픽 조회 (메인)", description = "메인 페이지에서 오늘의 톡픽을 조회합니다.")
+    @GetMapping("/today")
+    public ApiResponse<TodayTalkPickResponse> findTodayTalkPick(final Pageable pageable) {
+        return ApiResponse.ok(new TodayTalkPickResponse(1L, "제목", "요약", "O", "X"));
     }
 }
