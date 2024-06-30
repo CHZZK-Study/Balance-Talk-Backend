672c0b2d15a21ce980b06cd80a72b21a55b0d4d4
jschoi-96 <choix712@umn.edu>
2024-06-30 16:12:59 +0900

    chore: 게임 API 명세서 작성

--- src/main/java/balancetalk/game/dto/GameResponse.java
+++ src/main/java/balancetalk/game/dto/GameResponse.java
@@ -1,6 +1,5 @@
 package balancetalk.game.dto;
 
-import balancetalk.game.domain.Option;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,16 +15,9 @@ public class GameResponse {
     @Schema(description = "밸런스 게임 제목", example = "제목")
     private String title;
 
-    @Schema(description = "투표한 선택지 이름", example = "A")
-    private Option option;
+    @Schema(description = "선택지 A 이름", example = "선택지 A 이름")
+    private String optionA;
 
-    @Schema(description = "북마크 여부", example = "false")
-    private boolean myBookmark;
-
-    @Schema(description = "투표 여부", example = "true")
-    private boolean myVote;
-
-    @Schema(description = "이미지 URL",
-            example = "https://balance-talk-static-files4df23447-2355-45h2-8783-7f6gd2ceb848_고양이.jpg")
-    private String imageUrl;
+    @Schema(description = "선택지 B 이름", example = "선택지 B 이름")
+    private String optionB;
 }
