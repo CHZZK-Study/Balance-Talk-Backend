6e007aa618403478a0ba6084d289ddf6c2227e40
gywns0417 <gywns0417@gmail.com>
2024-06-30 23:23:46 +0900

    chore: LikeController API 명세 업데이트

--- src/main/java/balancetalk/like/presentation/LikeController.java
+++ src/main/java/balancetalk/like/presentation/LikeController.java
@@ -11,18 +11,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("likes/talks/{talkPickId}")
+@RequestMapping("/talks")
 @RequiredArgsConstructor
 @Tag(name = "like", description = "좋아요 API")
 public class LikeController {
-    @PostMapping("/{commentId}/likes")
+    @PostMapping("/{talkPickId}/{commentId}/likes")
     @Operation(summary = "댓글 좋아요", description = "talkPick-id에 해당하는 댓글에 추천을 누른다.")
     public ApiResponse<String> likeComment(@PathVariable Long talkPickId, @PathVariable Long commentId) {
         //commentService.likeComment(postId, commentId);
         return ApiResponse.ok("댓글 좋아요가 정상적으로 처리되었습니다.");
     }
 
-    @DeleteMapping("/{commentId}/likes")
+    @DeleteMapping("/{talkPickId}/{commentId}/likes")
     @Operation(summary = "댓글 좋아요 취소", description = "talkPick-id에 해당하는 댓글에 누른 추천을 취소한다.")
     public void cancelLikeComment(@PathVariable Long commentId) {
         //commentService.cancelLikeComment(commentId);
