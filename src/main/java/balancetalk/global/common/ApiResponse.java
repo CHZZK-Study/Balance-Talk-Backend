022c04a97e2e7208fe463919c35195436a642a36
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-06-30 16:21:22 +0900

    API 명세서 업데이트 (톡픽, 투표) (#351)
    * chore: 공통 응답 포맷 스웨거 설정 추가
    
    * chore: '톡픽 생성' API 명세 추가
    
    * refactor: 톡픽 생성 dto 이름 수정
    
    * chore: 오늘의 톡픽 API 명세 추가
    
    * chore: 톡픽 수정 API 명세 추가
    
    * chore: 톡픽 삭제 API 명세 추가
    
    * chore: 톡픽 상세 조회 API 명세 추가
    
    * refactor: 오늘의 톡픽 관련 API 컨트롤러 분리
    
    * chore: 톡픽 좋아요 API 명세 추가
    
    * refactor: "/today"를 컨트롤러 레벨로 수정
    
    * chore: 투표 생성 API 명세 추가
    
    * chore: 투표 결과 조회 API 명세 추가
    
    * chore: 투표 수정 API 명세 추가
    
    * chore: 파일 관련 API 임시 주석 처리

--- src/main/java/balancetalk/global/common/ApiResponse.java
+++ src/main/java/balancetalk/global/common/ApiResponse.java
@@ -1,17 +1,25 @@
 package balancetalk.global.common;
 
-import balancetalk.global.exception.ErrorCode;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 import static org.springframework.http.HttpStatus.OK;
 
+@Getter
 public class ApiResponse<T> {
 
     private static final String SUCCESS = "SUCCESS";
 
+    @Schema(description = "HTTP 상태 코드", example = "201")
     private int code;
+
+    @Schema(description = "HTTP 상태", example = "CREATED")
     private HttpStatus status;
+
+    @Schema(description = "HTTP 상태 메시지", example = "HTTP 상태 메시지")
     private String message;
+
     private T data;
 
     private ApiResponse(HttpStatus status, String message, T data) {
@@ -29,6 +37,10 @@ public class ApiResponse<T> {
 
     public static <T> ApiResponse<T> ok(T data) {
         return new ApiResponse<>(OK, SUCCESS, data);
+    }
+
+    public static <T> ApiResponse<T> ok() {
+        return new ApiResponse<>(OK, SUCCESS, null);
     }
 
     public static <T> ApiResponse<T> error(HttpStatus status, String message) {
