7912ac257a82d36c6dfd482e67d890f09a6b9fae
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-07-02 00:36:26 +0900

    refactor: ApiResponse 제거

--- src/main/java/balancetalk/game/presentation/GameController.java
+++ src/main/java/balancetalk/game/presentation/GameController.java
@@ -3,23 +3,16 @@ package balancetalk.game.presentation;
 import balancetalk.game.dto.GameDetailResponse;
 import balancetalk.game.dto.GameRequest;
 import balancetalk.game.dto.GameResponse;
-import balancetalk.global.common.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.util.Arrays;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Slf4j
 @RestController
