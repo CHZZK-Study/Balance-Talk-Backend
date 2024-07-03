cd9172dc66735b7c9904153acddebc5d91f63b37
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-07-04 02:10:06 +0900

    feat: 톡픽 북마크 관련 API 명세 추가

---
+++ src/main/java/balancetalk/bookmark/presentation/BookmarkTalkPickController.java
@@ -0,0 +1,21 @@
+package balancetalk.bookmark.presentation;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/bookmarks/talks/{talkPickId}")
+@Tag(name = "bookmark", description = "북마크 API")
+public class BookmarkTalkPickController {
+
+    @Operation(summary = "톡픽 북마크 추가", description = "북마크에 톡픽을 추가합니다.")
+    @PostMapping
+    public void bookmarkTalkPick(@PathVariable Long talkPickId) {
+    }
+
+    @Operation(summary = "톡픽 북마크 추가", description = "북마크에 톡픽을 추가합니다.")
+    @DeleteMapping
+    public void deleteBookmarkTalkPick(@PathVariable Long talkPickId) {
+    }
+}
