3b6e006adde92969b3bfd2db546f88a1e81fb9fc
gywns0417 <gywns0417@gmail.com>
2024-07-03 21:09:42 +0900

    feat: 댓글 좋아요 컨트롤러 추가 작성

--- src/main/java/balancetalk/like/presentation/LikeController.java
+++ src/main/java/balancetalk/like/presentation/LikeController.java
@@ -1,5 +1,6 @@
 package balancetalk.like.presentation;
 
+import balancetalk.like.application.CommentLikeService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -13,17 +14,19 @@ public class LikeController {
 
     private static final String SUCCESS_RESPONSE_MESSAGE = "OK";
 
+    private final CommentLikeService commentLikeService;
+
     @PostMapping("/{talkPickId}/{commentId}/likes")
     @Operation(summary = "댓글 좋아요", description = "commentId에 해당하는 댓글에 좋아요를 활성화합니다.")
     public String likeComment(@PathVariable Long commentId) {
-        //commentService.likeComment(postId, commentId);
+        commentLikeService.likeComment(commentId);
         return SUCCESS_RESPONSE_MESSAGE;
     }
 
     @DeleteMapping("/{talkPickId}/{commentId}/likes")
     @Operation(summary = "댓글 좋아요 취소", description = "commentId에 해당하는 댓글의 좋아요를 취소합니다.")
     public String cancelLikeComment(@PathVariable Long commentId) {
-        //commentService.cancelLikeComment(commentId);
+        commentLikeService.unLikeComment(commentId);
         return SUCCESS_RESPONSE_MESSAGE;
     }
 }
