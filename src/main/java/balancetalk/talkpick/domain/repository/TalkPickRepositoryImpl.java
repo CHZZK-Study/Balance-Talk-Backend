a91608ba2b474381c1743707e2bd8de29ee0f81d
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-07-05 22:35:08 +0900

    feat: DAO에 오늘의 톡픽 조회 메서드 추가

---
+++ src/main/java/balancetalk/talkpick/domain/repository/TalkPickRepositoryImpl.java
@@ -0,0 +1,30 @@
+package balancetalk.talkpick.domain.repository;
+
+import balancetalk.talkpick.dto.QTodayTalkPickDto_TodayTalkPickResponse;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+
+import static balancetalk.talkpick.domain.QTalkPick.talkPick;
+import static balancetalk.talkpick.dto.TodayTalkPickDto.TodayTalkPickResponse;
+import static balancetalk.vote.domain.QVote.vote;
+
+@RequiredArgsConstructor
+public class TalkPickRepositoryImpl implements TalkPickRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public TodayTalkPickResponse findTodayTalkPick() {
+        return queryFactory
+                .select(new QTodayTalkPickDto_TodayTalkPickResponse(
+                        talkPick.id, talkPick.title, talkPick.summary, talkPick.optionA, talkPick.optionB
+                ))
+                .from(vote)
+                .join(vote.talkPick, talkPick)
+                .where(talkPick.id.eq(vote.talkPick.id))
+                .groupBy(talkPick.id)
+                .orderBy(vote.count().desc())
+                .limit(1)
+                .fetchOne();
+    }
+}
