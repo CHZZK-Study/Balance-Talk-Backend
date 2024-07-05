c0fecc5b26b364074addce698d6656dc5a38d124
gywns0417 <gywns0417@gmail.com>
2024-07-06 01:02:03 +0900

    style: CommentController 코드 cleanup

--- src/main/java/balancetalk/comment/presentation/CommentController.java
+++ src/main/java/balancetalk/comment/presentation/CommentController.java
@@ -1,7 +1,8 @@
 package balancetalk.comment.presentation;
 
 import balancetalk.comment.application.CommentService;
-import balancetalk.comment.dto.CommentDto;
+import balancetalk.comment.dto.CommentDto.CreateCommentRequest;
+import balancetalk.comment.dto.CommentDto.UpdateCommentRequest;
 import balancetalk.comment.dto.CommentDto.CommentResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -11,7 +12,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -24,13 +24,13 @@ public class CommentController {
 
     @PostMapping
     @Operation(summary = "댓글 작성", description = "talkPick-id에 해당하는 게시글에 댓글을 작성한다.")
-    public void createComment(@PathVariable Long talkPickId, @Valid @RequestBody CommentDto.CreateCommentRequest createCommentRequest) {
+    public void createComment(@PathVariable Long talkPickId, @Valid @RequestBody CreateCommentRequest createCommentRequest) {
         commentService.createComment(createCommentRequest, talkPickId);
     }
 
     @PostMapping("/{commentId}/replies")
     @Operation(summary = "답글 작성", description = "commentId에 해당하는 댓글에 답글을 작성한다.")
-    public void createCommentReply(@PathVariable Long talkPickId, @PathVariable Long commentId, @Valid @RequestBody CommentDto.CreateCommentRequest createCommentRequest) {
+    public void createCommentReply(@PathVariable Long talkPickId, @PathVariable Long commentId, @Valid @RequestBody CreateCommentRequest createCommentRequest) {
         commentService.createCommentReply(createCommentRequest, talkPickId, commentId);
     }
 
@@ -50,21 +50,10 @@ public class CommentController {
         return commentService.findAllBestComments(talkPickId, pageable);
     }
 
-    /*
-    @ResponseStatus(HttpStatus.OK)
-    @GetMapping("/best")
-    @Operation(summary = "인기 댓글 조회", description = "추천 수가 가장 많은 댓글을 각 선택지별로 3개씩 조회한다.")
-    public List<BestCommentResponse> findBestComments(@PathVariable Long postId,
-                                                      @RequestHeader(value = "Authorization", required = false) String token) {
-        return null;
-    }
-
-     */
-
     @PutMapping("/{commentId}")
     @Operation(summary = "댓글 수정", description = "commentId에 해당하는 댓글 내용을 수정한다.")
     public void updateComment(@PathVariable Long commentId, @PathVariable Long talkPickId,
-                              @RequestBody CommentDto.UpdateCommentRequest request) {
+                              @RequestBody UpdateCommentRequest request) {
         commentService.updateComment(commentId, talkPickId, request.getContent());
     }
 
@@ -74,25 +63,4 @@ public class CommentController {
     public void deleteComment(@PathVariable Long commentId, @PathVariable Long talkPickId) {
         commentService.deleteComment(commentId, talkPickId);
     }
-
-    /*
-    @ResponseStatus(HttpStatus.OK)
-    @GetMapping("/{commentId}/replies")
-    @Operation(summary = "답글 목록 조회", description = "post-id 하위의 comment-id에 해당하는 댓글에 있는 모든 답글을 조회한다.")
-    public List<ReplyResponse> findAllReplies(@PathVariable Long postId, @PathVariable Long commentId,
-                                              @RequestHeader(value = "Authorization", required = false) String token) {
-        return commentService.findAllReplies(postId, commentId, token);
-    }
-    */
-    /*
-
-    @ResponseStatus(HttpStatus.OK)
-    @PostMapping("/{commentId}/report")
-    @Operation(summary = "댓글 신고", description = "comment-id에 해당하는 댓글을 신고 처리한다.")
-    public String reportComment(@PathVariable Long postId, @PathVariable Long commentId, @RequestBody ReportRequest reportRequest) {
-        commentService.reportComment(postId, commentId, reportRequest);
-        return "신고가 성공적으로 접수되었습니다.";
-    }
-
-     */
 }
