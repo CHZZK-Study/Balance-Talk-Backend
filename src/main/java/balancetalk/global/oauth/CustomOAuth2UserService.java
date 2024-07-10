8bede0e9db0072351b09100274ffbb7fe4d6d60b
gywns0417 <gywns0417@gmail.com>
2024-07-10 22:38:38 +0900

    chore: 소셜 로그인을 위한 클래스 내용 작성

--- src/main/java/balancetalk/global/oauth/CustomOAuth2UserService.java
+++ src/main/java/balancetalk/global/oauth/CustomOAuth2UserService.java
@@ -1,16 +1,67 @@
 package balancetalk.global.oauth;
 
+import balancetalk.member.domain.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
 
+    private final DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
+
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        return null;
+        // OAuth2UserService를 사용하여 사용자 정보를 가져옴
+        OAuth2User oAuth2User = delegate.loadUser(userRequest);
+
+        // 추가적인 사용자 정보 처리 로직
+        Map<String, Object> attributes = new HashMap<>(oAuth2User.getAttributes());
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+
+        // 소셜 로그인 제공자에 따라 사용자 정보를 가공
+        if ("kakao".equals(registrationId)) {
+            attributes = extractKakaoAttributes(attributes);
+        } else if ("naver".equals(registrationId)) {
+            attributes = extractNaverAttributes(attributes);
+        }
+
+        return new DefaultOAuth2User(
+                Collections.singleton(new OAuth2UserAuthority(attributes)),
+                attributes,
+                "email"
+        );
+    }
+
+    private Map<String, Object> extractKakaoAttributes(Map<String, Object> attributes) {
+        Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
+        String email = (String) kakaoAccount.get("email");
+        String nickname = (String) ((Map<String, Object>) kakaoAccount.get("profile")).get("nickname");
+
+        Map<String, Object> newAttributes = new HashMap<>();
+        newAttributes.put("email", email);
+        newAttributes.put("name", nickname);
+        return newAttributes;
+    }
+
+    private Map<String, Object> extractNaverAttributes(Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+        String email = (String) response.get("email");
+        String nickname = (String) response.get("nickname");
+
+        Map<String, Object> newAttributes = new HashMap<>();
+        newAttributes.put("email", email);
+        newAttributes.put("name", nickname);
+        return newAttributes;
     }
 }
--- src/main/java/balancetalk/global/oauth/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/balancetalk/global/oauth/OAuth2AuthenticationSuccessHandler.java
@@ -1,17 +1,73 @@
 package balancetalk.global.oauth;
 
+import balancetalk.global.jwt.JwtTokenProvider;
+import balancetalk.member.domain.Member;
+import balancetalk.member.domain.MemberRepository;
+import balancetalk.member.domain.Role;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.Optional;
 
+@Component
+@AllArgsConstructor
 public class OAuth2AuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final MemberRepository memberRepository;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        OAuth2AuthenticationToken token = (OAuth2AuthenticationToken) authentication;
+        OAuth2User oAuth2User = token.getPrincipal();
+        String registrationId = token.getAuthorizedClientRegistrationId();
 
+        // 사용자 정보 가져오기
+        String email = oAuth2User.getAttribute("email");
+        String name = oAuth2User.getAttribute("name");
+
+        Optional<Member> existingMember = memberRepository.findByEmail(email);
+        Member member;
+        if (existingMember.isEmpty()) {
+            member = Member.builder()
+                    .email(email)
+                    .nickname(name)
+                    .password("")  // 소셜 로그인은 비밀번호가 필요 없음
+                    .role(Role.USER)
+                    .build();
+            memberRepository.save(member);
+        } else {
+            member = existingMember.get();
+        }
+
+        // JWT 토큰 생성 및 반환
+        Authentication auth = new UsernamePasswordAuthenticationToken(member, null, member.getAuthorities());
+        String accessToken = jwtTokenProvider.createAccessToken(auth, member.getId());
+        String refreshToken = jwtTokenProvider.createRefreshToken(auth);
+
+        Cookie cookie = jwtTokenProvider.createCookie(refreshToken);
+        response.addCookie(cookie);
+
+        String redirectUrl = determineRedirectUrl(registrationId);
+        response.sendRedirect(redirectUrl + "?token=" + accessToken);
+    }
+
+    private String determineRedirectUrl(String registrationId) {
+        return switch (registrationId) {
+            case "google" -> "http://localhost:3000/google-success";
+            case "kakao" -> "http://localhost:3000/kakao-success";
+            case "naver" -> "http://localhost:3000/naver-success";
+            default -> throw new IllegalStateException("Unexpected value: " + registrationId);
+        };
     }
 }
