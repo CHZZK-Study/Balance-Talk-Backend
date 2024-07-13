4c2bb051b2b3a70982cd19f7e70a994c127f8e8e
jschoi-96 <choix712@umn.edu>
2024-07-13 17:54:42 +0900

    feat: 소셜 로그인으로 가입 시 임시 비밀번호가 인코딩되어 db에 저장되도록 구현

--- src/main/java/balancetalk/global/oauth2/service/CustomOAuth2UserService.java
+++ src/main/java/balancetalk/global/oauth2/service/CustomOAuth2UserService.java
@@ -1,5 +1,7 @@
 package balancetalk.global.oauth2.service;
 
+import static balancetalk.global.config.SecurityConfig.*;
+
 import balancetalk.global.oauth2.dto.CustomOAuth2User;
 import balancetalk.global.oauth2.dto.GoogleResponse;
 import balancetalk.global.oauth2.dto.KakaoResponse;
@@ -22,13 +24,14 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
+    private static final String TEMP_PASSWORD = "TEMP_PASSWORD";
     private final MemberRepository memberRepository;
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
 
         OAuth2User oAuth2User = super.loadUser(userRequest);
-        log.error("Loading user: {}", oAuth2User);
+        log.info("Loading user: {}", oAuth2User);
 
         String registrationId = userRequest.getClientRegistration().getRegistrationId();
 
@@ -54,11 +57,13 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
         Member findMember = memberRepository.findByUsername(username);
 
         if (findMember == null) {
+            String encodedPassword = passwordEncoder().encode(TEMP_PASSWORD);
             Oauth2Dto oauth2Dto = Oauth2Dto.builder()
                     .name(oauth2Response.getName())
                     .email(oauth2Response.getEmail())
                     .username(username)
                     .role(Role.USER)
+                    .password(encodedPassword)
                     .build();
 
             Member newMember = oauth2Dto.toEntity();
