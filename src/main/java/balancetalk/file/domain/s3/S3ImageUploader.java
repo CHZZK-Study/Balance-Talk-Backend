ecaa5555babc7eb34646d951b7f679651cdd3e67
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-07-14 14:47:10 +0900

    이미지 업로드 기능 단위 테스트 (#405)
    * test: FileProcessor 단위 테스트 작성
    
    * refactor: s3Client와 bucket의 위치를 FileService로 수정
    
    * chore: 서브모듈 업데이트
    
    * test: S3ImageUploader 단위 테스트
    
    * test: S3ImageRemover 단위 테스트
    
    * refactor: getUrl 메서드 이름을 getImageUrl로 수정
    
    * test: S3ImageUrlGetter 단위 테스트

--- src/main/java/balancetalk/file/domain/s3/S3ImageUploader.java
+++ src/main/java/balancetalk/file/domain/s3/S3ImageUploader.java
@@ -2,7 +2,6 @@ package balancetalk.file.domain.s3;
 
 import balancetalk.global.exception.BalanceTalkException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.core.sync.RequestBody;
@@ -18,20 +17,15 @@ import static balancetalk.global.exception.ErrorCode.FILE_UPLOAD_FAILED;
 @RequiredArgsConstructor
 public class S3ImageUploader {
 
-    private final S3Client s3Client;
-
-    @Value("${aws.s3.bucket}")
-    private String bucket;
-
-    public void uploadImageToBucket(MultipartFile multipartFile, String key) {
+    public void uploadImageToBucket(S3Client s3Client, String bucket, String key, MultipartFile multipartFile) {
         try (InputStream inputStream = multipartFile.getInputStream()) {
-            s3Client.putObject(getPutObjectRequest(key), getRequestBody(multipartFile, inputStream));
+            s3Client.putObject(getPutObjectRequest(bucket, key), getRequestBody(multipartFile, inputStream));
         } catch (IOException e) {
             throw new BalanceTalkException(FILE_UPLOAD_FAILED);
         }
     }
 
-    private PutObjectRequest getPutObjectRequest(String key) {
+    private PutObjectRequest getPutObjectRequest(String bucket, String key) {
         return PutObjectRequest.builder()
                 .bucket(bucket)
                 .key(key)
