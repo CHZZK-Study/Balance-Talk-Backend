efc622647ecca62ebb703790634f0f075fc362ba
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-07-15 23:22:49 +0900

    feat: 변경된 요구사항에 따라 오늘의 톡픽 조회 기능 수정

--- src/main/java/balancetalk/talkpick/domain/repository/TalkPickRepositoryImpl.java
+++ src/main/java/balancetalk/talkpick/domain/repository/TalkPickRepositoryImpl.java
@@ -23,7 +23,7 @@ public class TalkPickRepositoryImpl implements TalkPickRepositoryCustom {
                 .join(vote.talkPick, talkPick)
                 .where(talkPick.id.eq(vote.talkPick.id))
                 .groupBy(talkPick.id)
-                .orderBy(vote.count().desc(), talkPick.createdAt.desc())
+                .orderBy(talkPick.views.desc(), vote.count().desc(), talkPick.createdAt.desc())
                 .limit(1)
                 .fetchOne();
     }
