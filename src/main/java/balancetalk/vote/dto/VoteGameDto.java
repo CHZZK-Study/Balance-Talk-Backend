b841ed6f560fda314c5792343523716f5c304dd4
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-07-19 16:03:17 +0900

    refactor: VoteDto 분리

---
+++ src/main/java/balancetalk/vote/dto/VoteGameDto.java
@@ -0,0 +1,30 @@
+package balancetalk.vote.dto;
+
+import balancetalk.vote.domain.VoteOption;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+public class VoteGameDto {
+
+    @Data
+    @AllArgsConstructor
+    @Schema(description = "밸런스 게임 투표 생성 요청")
+    public static class VoteRequest {
+
+        @Schema(description = "투표할 선택지", example = "A")
+        private VoteOption voteOption;
+    }
+
+    @Data
+    @AllArgsConstructor
+    @Schema(description = "밸런스 게임 투표 결과 응답")
+    public static class VoteResultResponse {
+
+        @Schema(description = "선택지 B 투표 개수", example = "23")
+        private int optionACount;
+
+        @Schema(description = "선택지 B 투표 개수", example = "12")
+        private int optionBCount;
+    }
+}
--- src/main/java/balancetalk/vote/dto/VoteTalkPickDto.java
+++ src/main/java/balancetalk/vote/dto/VoteTalkPickDto.java
@@ -5,10 +5,11 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-public class VoteDto {
+public class VoteTalkPickDto {
 
     @Data
-    @Schema(description = "투표 생성 요청")
+    @AllArgsConstructor
+    @Schema(description = "톡픽 투표 생성 요청")
     public static class VoteRequest {
 
         @Schema(description = "투표할 선택지", example = "A")
@@ -17,8 +18,8 @@ public class VoteDto {
 
     @Data
     @AllArgsConstructor
-    @Schema(description = "투표 결과 응답")
-    public static class VoteResponse {
+    @Schema(description = "톡픽 투표 결과 응답")
+    public static class VoteResultResponse {
 
         @Schema(description = "선택지 B 투표 개수", example = "23")
         private int optionACount;
