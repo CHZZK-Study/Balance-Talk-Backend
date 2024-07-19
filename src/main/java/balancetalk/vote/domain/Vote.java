691a1aee5338ec956afa18b4fbc892f29e7cb01f
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-07-19 15:56:36 +0900

    refactor: Vote 엔티티에서 voteType 필드 제거

--- src/main/java/balancetalk/vote/domain/Vote.java
+++ src/main/java/balancetalk/vote/domain/Vote.java
@@ -6,21 +6,18 @@ import balancetalk.member.domain.Member;
 import balancetalk.talkpick.domain.TalkPick;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Vote extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Enumerated(value = EnumType.STRING)
-    private VoteType voteType;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
