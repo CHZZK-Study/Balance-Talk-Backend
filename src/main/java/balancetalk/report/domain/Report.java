1bbd93d5aa7d878f7c0de4551956f75a6a8f333a
king_0417 <73704053+gywns0417@users.noreply.github.com>
2024-07-19 19:27:12 +0900

    feat: Report-Comment 연관관계 제거

--- src/main/java/balancetalk/report/domain/Report.java
+++ src/main/java/balancetalk/report/domain/Report.java
@@ -1,15 +1,17 @@
 package balancetalk.report.domain;
 
-import balancetalk.comment.domain.Comment;
 import balancetalk.global.common.BaseTimeEntity;
 import balancetalk.member.domain.Member;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -27,6 +29,12 @@ public class Report extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Enumerated(value = EnumType.STRING)
+    private ReportType reportType;
+
+    @NotNull
+    private Long resourceId;
+
     @NotBlank
     private String reason;
 
@@ -38,6 +46,7 @@ public class Report extends BaseTimeEntity {
     private Member reporter;
 
     @ManyToOne
-    @JoinColumn(name = "comment_id")
-    private Comment comment;
+    @JoinColumn(name = "reported_id")
+    private Member reported;
+
 }
