90492f2cec682a46266a08cac2f4fe23796a71ee
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-07-21 01:19:32 +0900

    톡픽 투표 기능 구현 (#422)
    * refactor: Vote 엔티티에서 voteType 필드 제거
    
    * refactor: VoteDto 분리
    
    * fix: VoteDto 분리에 따른 코드 수정
    
    * fix: Vote 엔티티의 voteOption에서 @NotBlank 제거
    
    * feat: 톡픽 투표 생성 기능 구현
    
    * feat: 톡픽 투표 생성 메서드에 @Transactional 추가
    
    * feat: 톡픽 투표 수정 기능 구현
    
    * feat: 톡픽 투표 삭제 기능 구현
    
    * refactor: talkPickId 원시 타입으로 변경
    
    * style: 불필요한 import문 제거
    
    * chore: spring-fox 의존성 제거
    
    * style: SwaggerConfig 정렬
    
    * chore: springdoc-openapi-ui 의존성 제거
    
    * fix: Swagger UI에서 @AuthPrincipal 파라미터 제거

--- src/main/java/balancetalk/vote/domain/Vote.java
+++ src/main/java/balancetalk/vote/domain/Vote.java
@@ -5,22 +5,18 @@ import balancetalk.global.common.BaseTimeEntity;
 import balancetalk.member.domain.Member;
 import balancetalk.talkpick.domain.TalkPick;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Vote extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Enumerated(value = EnumType.STRING)
-    private VoteType voteType;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
@@ -34,11 +30,14 @@ public class Vote extends BaseTimeEntity {
     @JoinColumn(name = "game_id")
     private Game game;
 
-    @NotBlank
     @Enumerated(value = EnumType.STRING)
     private VoteOption voteOption;
 
     public boolean matchesTalkPick(TalkPick talkPick) {
         return this.talkPick.equals(talkPick);
+    }
+
+    public void updateVoteOption(VoteOption newVoteOption) {
+        this.voteOption = newVoteOption;
     }
 }
--- src/main/java/balancetalk/vote/presentation/VoteTalkPickController.java
+++ src/main/java/balancetalk/vote/presentation/VoteTalkPickController.java
@@ -1,31 +1,53 @@
 package balancetalk.vote.presentation;
 
-import balancetalk.vote.dto.VoteDto.VoteRequest;
-import balancetalk.vote.dto.VoteDto.VoteResponse;
+import balancetalk.global.utils.AuthPrincipal;
+import balancetalk.member.dto.ApiMember;
+import balancetalk.member.dto.GuestOrApiMember;
+import balancetalk.vote.application.VoteTalkPickService;
+import balancetalk.vote.dto.VoteTalkPickDto.VoteResultResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import static balancetalk.vote.dto.VoteTalkPickDto.VoteRequest;
+
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/votes/talks/{talkPickId}")
 @Tag(name = "vote", description = "투표 API")
 public class VoteTalkPickController {
 
+    private final VoteTalkPickService voteTalkPickService;
+
     @Operation(summary = "톡픽 투표 생성", description = "톡픽에서 원하는 선택지에 투표합니다.")
     @PostMapping
-    public void createVoteTalkPick(@PathVariable Long talkPickId,
-                                   @RequestBody VoteRequest request) {
+    public void createVoteTalkPick(@PathVariable long talkPickId,
+                                   @RequestBody VoteRequest request,
+                                   @Parameter(hidden = true) @AuthPrincipal GuestOrApiMember guestOrApiMember) {
+        voteTalkPickService.createVote(talkPickId, request, guestOrApiMember);
     }
 
+    // TODO 톡픽 조회에 포함하기
     @Operation(summary = "톡픽 투표 결과 조회", description = "톡픽 투표 결과를 조회합니다.")
     @GetMapping
-    public VoteResponse getVoteResultTalkPick(@PathVariable Long talkPickId) {
-        return new VoteResponse(23, 12);
+    public VoteResultResponse getVoteResultTalkPick(@PathVariable Long talkPickId) {
+        return new VoteResultResponse(23, 12);
     }
 
     @Operation(summary = "톡픽 투표 수정", description = "톡픽 투표를 수정합니다.")
     @PutMapping
-    public void updateVoteResultTalkPick(@PathVariable Long talkPickId,
-                                         @RequestBody VoteRequest request) {
+    public void updateVoteResultTalkPick(@PathVariable long talkPickId,
+                                         @RequestBody VoteRequest request,
+                                         @Parameter(hidden = true) @AuthPrincipal ApiMember apiMember) {
+        voteTalkPickService.updateVote(talkPickId, request, apiMember);
+    }
+
+    @Operation(summary = "톡픽 투표 삭제", description = "톡픽 투표를 삭제합니다.")
+    @DeleteMapping
+    public void deleteVoteTalkPick(@PathVariable long talkPickId,
+                                   @Parameter(hidden = true) @AuthPrincipal ApiMember apiMember) {
+        voteTalkPickService.deleteVote(talkPickId, apiMember);
     }
 }
