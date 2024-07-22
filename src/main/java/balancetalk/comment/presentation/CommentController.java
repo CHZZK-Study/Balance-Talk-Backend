2b59943fc2e7a5565d7e7661df39bdc2e0045cfd
king_0417 <73704053+gywns0417@users.noreply.github.com>
2024-07-22 20:01:55 +0900

    feat: ApiMember 스웨거 hidden 처리 (#429)

--- src/main/java/balancetalk/comment/presentation/CommentController.java
+++ src/main/java/balancetalk/comment/presentation/CommentController.java
@@ -8,6 +8,7 @@ import balancetalk.global.utils.AuthPrincipal;
 import balancetalk.member.dto.ApiMember;
 import balancetalk.member.dto.GuestOrApiMember;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -28,7 +29,7 @@ public class CommentController {
     @PostMapping
     @Operation(summary = "댓글 작성", description = "talkPick-id에 해당하는 게시글에 댓글을 작성한다.")
     public void createComment(@PathVariable Long talkPickId, @Valid @RequestBody CreateCommentRequest createCommentRequest,
-                              @AuthPrincipal ApiMember apiMember) {
+                              @Parameter(hidden = true) @AuthPrincipal ApiMember apiMember) {
         commentService.createComment(createCommentRequest, talkPickId, apiMember);
     }
 
@@ -36,14 +37,14 @@ public class CommentController {
     @Operation(summary = "답글 작성", description = "commentId에 해당하는 댓글에 답글을 작성한다.")
     public void createCommentReply(@PathVariable Long talkPickId, @PathVariable Long commentId,
                                    @Valid @RequestBody CreateCommentRequest createCommentRequest,
-                                   @AuthPrincipal ApiMember apiMember) {
+                                   @Parameter(hidden = true) @AuthPrincipal ApiMember apiMember) {
         commentService.createCommentReply(createCommentRequest, talkPickId, commentId, apiMember);
     }
 
     @GetMapping
     @Operation(summary = "최신 댓글 목록 조회", description = "talkPick-id에 해당하는 게시글에 있는 모든 댓글 및 답글을 최신순으로 정렬해 조회한다.")
     public Page<CommentResponse> findAllCommentsByPostIdSortedByCreatedAt(@PathVariable Long talkPickId, Pageable pageable,
-                                                                          @AuthPrincipal GuestOrApiMember guestOrApiMember) {
+                                                                          @Parameter(hidden = true) @AuthPrincipal GuestOrApiMember guestOrApiMember) {
         Pageable sortedByCreatedAtDesc = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(),
                 Sort.by("createdAt").descending());
         return commentService.findAllComments(talkPickId, sortedByCreatedAtDesc, guestOrApiMember);
@@ -52,21 +53,21 @@ public class CommentController {
     @GetMapping("/best")
     @Operation(summary = "베스트 댓글 목록 조회", description = "talkPick-id에 해당하는 게시글에 있는 모든 댓글 및 답글을 베스트 및 좋아요 순으로 정렬해 조회한다.")
     public Page<CommentResponse> findAllBestCommentsByPostId(@PathVariable Long talkPickId, Pageable pageable,
-                                                             @AuthPrincipal GuestOrApiMember guestOrApiMember) {
+                                                             @Parameter(hidden = true) @AuthPrincipal GuestOrApiMember guestOrApiMember) {
         return commentService.findAllBestComments(talkPickId, pageable, guestOrApiMember);
     }
 
     @PutMapping("/{commentId}")
     @Operation(summary = "댓글 수정", description = "commentId에 해당하는 댓글 내용을 수정한다.")
     public void updateComment(@PathVariable Long commentId, @PathVariable Long talkPickId,
-                              @RequestBody UpdateCommentRequest request, @AuthPrincipal ApiMember apiMember) {
+                              @RequestBody UpdateCommentRequest request, @Parameter(hidden = true) @AuthPrincipal ApiMember apiMember) {
         commentService.updateComment(commentId, talkPickId, request.getContent(), apiMember);
     }
 
 
     @DeleteMapping("/{commentId}")
     @Operation(summary = "댓글 삭제", description = "commentId에 해당하는 댓글을 삭제한다.")
-    public void deleteComment(@PathVariable Long commentId, @PathVariable Long talkPickId, @AuthPrincipal ApiMember apiMember) {
+    public void deleteComment(@PathVariable Long commentId, @PathVariable Long talkPickId, @Parameter(hidden = true) @AuthPrincipal ApiMember apiMember) {
         commentService.deleteComment(commentId, talkPickId, apiMember);
     }
 }
--- src/main/java/balancetalk/like/presentation/LikeController.java
+++ src/main/java/balancetalk/like/presentation/LikeController.java
@@ -4,6 +4,7 @@ import balancetalk.global.utils.AuthPrincipal;
 import balancetalk.like.application.CommentLikeService;
 import balancetalk.member.dto.ApiMember;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -18,13 +19,13 @@ public class LikeController {
 
     @PostMapping
     @Operation(summary = "댓글 좋아요", description = "commentId에 해당하는 댓글에 좋아요를 활성화합니다.")
-    public void likeComment(@PathVariable Long commentId, @PathVariable Long talkPickId, @AuthPrincipal ApiMember apiMember) {
+    public void likeComment(@PathVariable Long commentId, @PathVariable Long talkPickId, @Parameter(hidden = true) @AuthPrincipal ApiMember apiMember) {
         commentLikeService.likeComment(commentId, talkPickId, apiMember);
     }
 
     @DeleteMapping
     @Operation(summary = "댓글 좋아요 취소", description = "commentId에 해당하는 댓글의 좋아요를 취소합니다.")
-    public void unlikeComment(@PathVariable Long commentId, @PathVariable Long talkPickId, @AuthPrincipal ApiMember apiMember) {
+    public void unlikeComment(@PathVariable Long commentId, @PathVariable Long talkPickId, @Parameter(hidden = true) @AuthPrincipal ApiMember apiMember) {
         commentLikeService.unLikeComment(commentId, talkPickId, apiMember);
     }
 }
