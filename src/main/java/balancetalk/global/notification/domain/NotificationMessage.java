19d8738a6ac0f42903a4dcacb263e4afc68584ee
gywns0417 <gywns0417@gmail.com>
2024-08-01 15:09:47 +0900

    feat: 댓글 좋아요 알림 메시지 추가

--- src/main/java/balancetalk/global/notification/domain/NotificationMessage.java
+++ src/main/java/balancetalk/global/notification/domain/NotificationMessage.java
@@ -7,7 +7,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public enum NotificationMessage {
     FIRST_COMMENT_REPLY("내가 작성한 댓글에 첫 답글이 달렸어요!"),
-    COMMENT_REPLY("내가 작성한 댓글에 %d개의 답글이 달렸어요!");
+    COMMENT_REPLY("내가 작성한 댓글에 %d개의 답글이 달렸어요!"),
+    COMMENT_LIKE("내가 작성한 댓글이 %d개의 좋아요를 받았어요!");
 
     private final String message;
 
