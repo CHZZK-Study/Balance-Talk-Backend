f0967aaa8229f6186be62a70bcb53f9af35bcc34
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-08-02 22:22:05 +0900

    test: BookmarkGenerator 단위 테스트

---
+++ src/test/java/balancetalk/bookmark/domain/BookmarkGeneratorTest.java
@@ -0,0 +1,44 @@
+package balancetalk.bookmark.domain;
+
+import balancetalk.member.domain.Member;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static balancetalk.bookmark.domain.BookmarkType.TALK_PICK;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BookmarkGeneratorTest {
+
+    BookmarkGenerator bookmarkGenerator;
+    Member member;
+
+    @BeforeEach
+    void setUp() {
+        bookmarkGenerator = new BookmarkGenerator();
+        member = Member.builder()
+                .id(1L)
+                .build();
+    }
+
+    @Test
+    @DisplayName("북마크 객체를 성공적으로 생성합니다.")
+    void generate_Success_ThenReturnBookmark() {
+        // when
+        Bookmark bookmark = bookmarkGenerator.generate(1L, TALK_PICK, member);
+
+        // then
+        assertThat(bookmark.getResourceId()).isEqualTo(1L);
+        assertThat(bookmark.getBookmarkType()).isEqualTo(TALK_PICK);
+        assertThat(bookmark.getMember()).isEqualTo(member);
+    }
+
+    @Test
+    void generate_Success_ThenActiveIsTrue() {
+        // when
+        Bookmark bookmark = bookmarkGenerator.generate(1L, TALK_PICK, member);
+
+        // then
+        assertThat(bookmark.getActive()).isTrue();
+    }
+}
