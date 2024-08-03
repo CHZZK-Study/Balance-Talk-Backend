1c6957cbca0deb0048ae9e9a9f8aeb95aa877741
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-08-04 03:27:59 +0900

    style: 코드 정렬

--- src/main/java/balancetalk/talkpick/domain/repository/TalkPickRepositoryCustom.java
+++ src/main/java/balancetalk/talkpick/domain/repository/TalkPickRepositoryCustom.java
@@ -1,10 +1,9 @@
 package balancetalk.talkpick.domain.repository;
 
-import balancetalk.talkpick.dto.TalkPickDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import static balancetalk.talkpick.dto.TalkPickDto.*;
+import static balancetalk.talkpick.dto.TalkPickDto.TalkPickResponse;
 import static balancetalk.talkpick.dto.TodayTalkPickDto.TodayTalkPickResponse;
 
 public interface TalkPickRepositoryCustom {
