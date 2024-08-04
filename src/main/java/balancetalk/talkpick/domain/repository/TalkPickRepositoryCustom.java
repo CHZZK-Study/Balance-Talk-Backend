cae8543a18d2156084fc32af1cf91fdc65f80eec
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-08-04 23:27:43 +0900

    톡픽 목록 조회 기능 구현 (#459)
    * refactor: Bookmark 카운트 쿼리 수정
    
    * feat: 톡픽 목록 조회 기능 구현
    
    * style: 코드 정렬

--- src/main/java/balancetalk/talkpick/domain/repository/TalkPickRepositoryCustom.java
+++ src/main/java/balancetalk/talkpick/domain/repository/TalkPickRepositoryCustom.java
@@ -1,8 +1,14 @@
 package balancetalk.talkpick.domain.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import static balancetalk.talkpick.dto.TalkPickDto.TalkPickResponse;
 import static balancetalk.talkpick.dto.TodayTalkPickDto.TodayTalkPickResponse;
 
 public interface TalkPickRepositoryCustom {
 
     TodayTalkPickResponse findTodayTalkPick();
+
+    Page<TalkPickResponse> findPagedTalkPicks(Pageable pageable);
 }
--- src/main/java/balancetalk/talkpick/presentation/TalkPickController.java
+++ src/main/java/balancetalk/talkpick/presentation/TalkPickController.java
@@ -7,10 +7,11 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
-import static balancetalk.talkpick.dto.TalkPickDto.CreateTalkPickRequest;
-import static balancetalk.talkpick.dto.TalkPickDto.TalkPickDetailResponse;
+import static balancetalk.talkpick.dto.TalkPickDto.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,12 +26,17 @@ public class TalkPickController {
     public void createTalkPick(@RequestBody final CreateTalkPickRequest request) {
     }
 
-    // TODO 투표수 포함
     @Operation(summary = "톡픽 상세 조회", description = "톡픽 상세 페이지를 조회합니다.")
     @GetMapping("/{talkPickId}")
     public TalkPickDetailResponse findTalkPickDetail(@PathVariable final Long talkPickId,
                                                      @Parameter(hidden = true) @AuthPrincipal final GuestOrApiMember guestOrApiMember) {
         return talkPickService.findById(talkPickId, guestOrApiMember);
+    }
+
+    @Operation(summary = "톡픽 목록 조회", description = "톡픽 목록을 조회합니다.")
+    @GetMapping
+    public Page<TalkPickResponse> findPagedTalkPicks(Pageable pageable) {
+        return talkPickService.findPaged(pageable);
     }
 
     @Operation(summary = "톡픽 수정", description = "톡픽을 수정합니다.")
