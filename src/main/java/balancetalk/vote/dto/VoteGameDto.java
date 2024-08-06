f38b81b18edec6b2867c9ab73ebf536d38b9b0a6
jschoi-96 <choix712@umn.edu>
2024-08-07 02:07:51 +0900

    feat: 밸런스 게임 투표 생성, 수정, 삭제 기능 구현

--- src/main/java/balancetalk/vote/dto/VoteGameDto.java
+++ src/main/java/balancetalk/vote/dto/VoteGameDto.java
@@ -1,19 +1,33 @@
 package balancetalk.vote.dto;
 
+import balancetalk.game.domain.Game;
+import balancetalk.member.domain.Member;
+import balancetalk.vote.domain.Vote;
 import balancetalk.vote.domain.VoteOption;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 
 public class VoteGameDto {
 
     @Data
+    @NoArgsConstructor
     @AllArgsConstructor
     @Schema(description = "밸런스 게임 투표 생성 요청")
     public static class VoteRequest {
 
         @Schema(description = "투표할 선택지", example = "A")
         private VoteOption voteOption;
+
+        public Vote toEntity(Member member, Game game) {
+            return Vote.builder()
+                    .member(member)
+                    .game(game)
+                    .voteOption(voteOption)
+                    .build();
+        }
     }
 
     @Data
