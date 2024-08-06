310a5f87cf475fc35a7bb720fd75fb456ea589e4
jschoi-96 <choix712@umn.edu>
2024-08-06 21:27:29 +0900

    feat: jpql을 사용하여 카테고리 별로 최신순, 조회순 정렬하도록 구현

--- src/main/java/balancetalk/game/domain/repository/GameRepository.java
+++ src/main/java/balancetalk/game/domain/repository/GameRepository.java
@@ -1,14 +1,18 @@
 package balancetalk.game.domain.repository;
 
 import balancetalk.game.domain.Game;
-import balancetalk.game.dto.GameDto.GameResponse;
-import org.springframework.data.domain.Page;
+import java.util.List;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface GameRepository extends JpaRepository<Game, Long> {
 
-    Page<GameResponse> findAllByOrderByCreatedAtDesc(Pageable pageable);
+    @Query("SELECT g FROM Game g WHERE g.gameTopic.id = :gameTopicId ORDER BY g.createdAt DESC")
+    List<Game> findGamesByCreated(@Param("gameTopicId") int gameTopicId, Pageable pageable);
 
-    Page<GameResponse> findAllByOrderByViewsDesc(Pageable pageable);
+    @Query("SELECT g FROM Game g WHERE g.gameTopic.id = :gameTopicId ORDER BY g.views DESC")
+    List<Game> findGamesByViews(@Param("gameTopicId") int gameTopicId, Pageable pageable);
+
 }
