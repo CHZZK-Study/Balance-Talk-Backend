db1b09bc964303d293b4d43b368aaeea5fb12d6f
jschoi-96 <choix712@umn.edu>
2024-08-06 17:08:28 +0900

    chore: 캐싱 기능 활성화

--- src/main/java/balancetalk/BalanceTalkApplication.java
+++ src/main/java/balancetalk/BalanceTalkApplication.java
@@ -4,10 +4,12 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @EnableJpaAuditing
+@EnableCaching
 @SpringBootApplication
 public class BalanceTalkApplication {
 
