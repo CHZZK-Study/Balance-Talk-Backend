f48ffa3804891a206e654f0363e06da7f41e237c
gywns0417 <gywns0417@gmail.com>
2024-08-07 16:47:53 +0900

    feat: 북마크한 톡픽 조회 쿼리 작성

--- src/main/java/balancetalk/talkpick/domain/repository/TalkPickRepository.java
+++ src/main/java/balancetalk/talkpick/domain/repository/TalkPickRepository.java
@@ -2,6 +2,13 @@ package balancetalk.talkpick.domain.repository;
 
 import balancetalk.talkpick.domain.TalkPick;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface TalkPickRepository extends JpaRepository<TalkPick, Long>, TalkPickRepositoryCustom {
+
+    @Query("SELECT t FROM TalkPick t WHERE t.id IN :ids ORDER BY t.createdAt DESC")
+    List<TalkPick> findByIdInOrderByCreatedAtDesc(@Param("ids") List<Long> ids);
 }
