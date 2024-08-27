2686210bb10c827eeba3921322e582b266d82e8c
king_0417 <73704053+gywns0417@users.noreply.github.com>
2024-08-28 01:13:10 +0900

    feat: 내가 작성한 톡픽 투표 수 알림 기능 구현

--- src/main/java/balancetalk/vote/application/VoteTalkPickService.java
+++ src/main/java/balancetalk/vote/application/VoteTalkPickService.java
@@ -1,7 +1,17 @@
 package balancetalk.vote.application;
 
+import static balancetalk.global.notification.domain.NotificationMessage.TALK_PICK_VOTE;
+import static balancetalk.global.notification.domain.NotificationMessage.TALK_PICK_VOTE_100;
+import static balancetalk.global.notification.domain.NotificationMessage.TALK_PICK_VOTE_1000;
+import static balancetalk.global.notification.domain.NotificationStandard.FIRST_STANDARD_OF_NOTIFICATION;
+import static balancetalk.global.notification.domain.NotificationStandard.FOURTH_STANDARD_OF_NOTIFICATION;
+import static balancetalk.global.notification.domain.NotificationStandard.SECOND_STANDARD_OF_NOTIFICATION;
+import static balancetalk.global.notification.domain.NotificationStandard.THIRD_STANDARD_OF_NOTIFICATION;
+import static balancetalk.global.notification.domain.NotificationTitleCategory.WRITTEN_TALK_PICK;
+
 import balancetalk.global.exception.BalanceTalkException;
 import balancetalk.global.exception.ErrorCode;
+import balancetalk.global.notification.application.NotificationService;
 import balancetalk.member.domain.Member;
 import balancetalk.member.domain.MemberRepository;
 import balancetalk.member.dto.ApiMember;
@@ -11,6 +21,7 @@ import balancetalk.talkpick.domain.TalkPickReader;
 import balancetalk.vote.domain.Vote;
 import balancetalk.vote.domain.VoteRepository;
 import balancetalk.vote.dto.VoteTalkPickDto.VoteRequest;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,6 +35,7 @@ public class VoteTalkPickService {
     private final TalkPickReader talkPickReader;
     private final VoteRepository voteRepository;
     private final MemberRepository memberRepository;
+    private final NotificationService notificationService;
 
     @Transactional
     public void createVote(long talkPickId, VoteRequest request, GuestOrApiMember guestOrApiMember) {
@@ -40,6 +52,7 @@ public class VoteTalkPickService {
         }
 
         voteRepository.save(request.toEntity(member, talkPick));
+        sendVoteTalkPickNotification(talkPick);
     }
 
     @Transactional
@@ -66,5 +79,36 @@ public class VoteTalkPickService {
         }
 
         voteRepository.delete(vote.get());
+    }
+
+    private void sendVoteTalkPickNotification(TalkPick talkPick) {
+        Member member = talkPick.getMember();
+        long votedCount = talkPick.getVotes().size();
+        String voteCountKey = "VOTE_" + votedCount;
+        Map<String, Boolean> notificationHistory = talkPick.getNotificationHistory();
+        String category = WRITTEN_TALK_PICK.getCategory();
+
+        boolean isMilestoneBookmarked = (votedCount == FIRST_STANDARD_OF_NOTIFICATION.getCount() ||
+                votedCount == SECOND_STANDARD_OF_NOTIFICATION.getCount() ||
+                votedCount == THIRD_STANDARD_OF_NOTIFICATION.getCount() ||
+                (votedCount > THIRD_STANDARD_OF_NOTIFICATION.getCount() &&
+                        votedCount % THIRD_STANDARD_OF_NOTIFICATION.getCount() == 0) ||
+                (votedCount > FOURTH_STANDARD_OF_NOTIFICATION.getCount() &&
+                        votedCount % FOURTH_STANDARD_OF_NOTIFICATION.getCount() == 0));
+
+        // 투표 개수가 10, 50, 100*n개, 1000*n개 일 때 알림
+        if (isMilestoneBookmarked && !notificationHistory.getOrDefault(voteCountKey, false)) {
+            notificationService.sendTalkPickNotification(member, talkPick, category, TALK_PICK_VOTE.format(votedCount));
+            // 투표 개수가 100개일 때 배찌 획득 알림
+            if (votedCount == THIRD_STANDARD_OF_NOTIFICATION.getCount()) {
+                notificationService.sendTalkPickNotification(member, talkPick, category, TALK_PICK_VOTE_100.getMessage());
+            }
+            // 투표 개수가 1000개일 때 배찌 획득 알림
+            else if (votedCount == FOURTH_STANDARD_OF_NOTIFICATION.getCount()) {
+                notificationService.sendTalkPickNotification(member, talkPick, category, TALK_PICK_VOTE_1000.getMessage());
+            }
+            notificationHistory.put(voteCountKey, true);
+            talkPick.setNotificationHistory(notificationHistory);
+        }
     }
 }
