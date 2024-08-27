ba800c9fe899d98928a87d3fa8d7cc69223a39d6
king_0417 <73704053+gywns0417@users.noreply.github.com>
2024-08-28 01:06:01 +0900

    feat: 내가 작성한 톡픽 저장 수 알림 기능 구현

--- src/main/java/balancetalk/bookmark/application/BookmarkTalkPickService.java
+++ src/main/java/balancetalk/bookmark/application/BookmarkTalkPickService.java
@@ -4,17 +4,31 @@ import balancetalk.bookmark.domain.Bookmark;
 import balancetalk.bookmark.domain.BookmarkGenerator;
 import balancetalk.bookmark.domain.BookmarkRepository;
 import balancetalk.global.exception.BalanceTalkException;
+import balancetalk.global.notification.application.NotificationService;
 import balancetalk.member.domain.Member;
 import balancetalk.member.domain.MemberRepository;
 import balancetalk.member.dto.ApiMember;
 import balancetalk.talkpick.domain.TalkPick;
 import balancetalk.talkpick.domain.TalkPickReader;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import static balancetalk.bookmark.domain.BookmarkType.TALK_PICK;
 import static balancetalk.global.exception.ErrorCode.*;
+import static balancetalk.global.notification.domain.NotificationMessage.COMMENT_LIKE;
+import static balancetalk.global.notification.domain.NotificationMessage.COMMENT_LIKE_100;
+import static balancetalk.global.notification.domain.NotificationMessage.COMMENT_LIKE_1000;
+import static balancetalk.global.notification.domain.NotificationMessage.TALK_PICK_BOOKMARK;
+import static balancetalk.global.notification.domain.NotificationMessage.TALK_PICK_BOOKMARK_100;
+import static balancetalk.global.notification.domain.NotificationMessage.TALK_PICK_BOOKMARK_1000;
+import static balancetalk.global.notification.domain.NotificationStandard.FIRST_STANDARD_OF_NOTIFICATION;
+import static balancetalk.global.notification.domain.NotificationStandard.FOURTH_STANDARD_OF_NOTIFICATION;
+import static balancetalk.global.notification.domain.NotificationStandard.SECOND_STANDARD_OF_NOTIFICATION;
+import static balancetalk.global.notification.domain.NotificationStandard.THIRD_STANDARD_OF_NOTIFICATION;
+import static balancetalk.global.notification.domain.NotificationTitleCategory.OTHERS_TALK_PICK;
+import static balancetalk.global.notification.domain.NotificationTitleCategory.WRITTEN_TALK_PICK;
 
 @Service
 @RequiredArgsConstructor
@@ -24,6 +38,7 @@ public class BookmarkTalkPickService {
     private final MemberRepository memberRepository;
     private final BookmarkGenerator bookmarkGenerator;
     private final BookmarkRepository bookmarkRepository;
+    private final NotificationService notificationService;
 
     @Transactional
     public void createBookmark(final long talkPickId, final ApiMember apiMember) {
@@ -41,6 +56,7 @@ public class BookmarkTalkPickService {
                 .ifPresentOrElse(Bookmark::activate,
                         () -> bookmarkRepository.save(bookmarkGenerator.generate(talkPickId, TALK_PICK, member)));
         talkPick.increaseBookmarks();
+        sendBookmarkTalkPickNotification(talkPick);
     }
 
     @Transactional
@@ -61,5 +77,36 @@ public class BookmarkTalkPickService {
 
     private boolean isNotActivated(Bookmark bookmark) {
         return !bookmark.isActive();
+    }
+
+    private void sendBookmarkTalkPickNotification(TalkPick talkPick) {
+        Member member = talkPick.getMember();
+        long bookmarkedCount = talkPick.getBookmarks();
+        String bookmarkCountKey = "BOOKMARK_" + bookmarkedCount;
+        Map<String, Boolean> notificationHistory = talkPick.getNotificationHistory();
+        String category = WRITTEN_TALK_PICK.getCategory();
+
+        boolean isMilestoneBookmarked = (bookmarkedCount == 1 ||
+                bookmarkedCount == SECOND_STANDARD_OF_NOTIFICATION.getCount() ||
+                bookmarkedCount == THIRD_STANDARD_OF_NOTIFICATION.getCount() ||
+                (bookmarkedCount > THIRD_STANDARD_OF_NOTIFICATION.getCount() &&
+                        bookmarkedCount % THIRD_STANDARD_OF_NOTIFICATION.getCount() == 0) ||
+                (bookmarkedCount > FOURTH_STANDARD_OF_NOTIFICATION.getCount() &&
+                        bookmarkedCount % FOURTH_STANDARD_OF_NOTIFICATION.getCount() == 0));
+
+        // 북마크 개수가 10, 50, 100*n개, 1000*n개 일 때 알림
+        if (isMilestoneBookmarked && !notificationHistory.getOrDefault(bookmarkCountKey, false)) {
+            notificationService.sendTalkPickNotification(member, talkPick, category, TALK_PICK_BOOKMARK.format(bookmarkedCount));
+            // 북마크 개수가 100개일 때 배찌 획득 알림
+            if (bookmarkedCount == THIRD_STANDARD_OF_NOTIFICATION.getCount()) {
+                notificationService.sendTalkPickNotification(member, talkPick, category, TALK_PICK_BOOKMARK_100.getMessage());
+            }
+            // 북마크 개수가 1000개일 때 배찌 획득 알림
+            else if (bookmarkedCount == FOURTH_STANDARD_OF_NOTIFICATION.getCount()) {
+                notificationService.sendTalkPickNotification(member, talkPick, category, TALK_PICK_BOOKMARK_1000.getMessage());
+            }
+            notificationHistory.put(bookmarkCountKey, true);
+            talkPick.setNotificationHistory(notificationHistory);
+        }
     }
 }
