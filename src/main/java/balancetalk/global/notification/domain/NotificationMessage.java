a8fc4af3019808bc3363e6efb531fb48812b9303
king_0417 <73704053+gywns0417@users.noreply.github.com>
2024-08-28 01:12:32 +0900

    feat: 내가 작성한 톡픽 투표 개수 알림 메시지 작성

--- src/main/java/balancetalk/global/notification/domain/NotificationMessage.java
+++ src/main/java/balancetalk/global/notification/domain/NotificationMessage.java
@@ -13,7 +13,10 @@ public enum NotificationMessage {
     COMMENT_LIKE_1000("1000개 : 작성한 댓글에 하트가 1000개! ‘공감대왕 (캐릭터) 배찌를 얻었어요!"),
     TALK_PICK_BOOKMARK("작성한 톡픽이 저장 %d개를 달성했어요!"),
     TALK_PICK_BOOKMARK_100("작성한 톡픽을 100명이나 저장! ‘맛깔난 글솜씨 (캐릭터) 배찌를 얻었어요!"),
-    TALK_PICK_BOOKMARK_1000("작성한 톡픽을 1000명이나 저장! ‘ 킹왕짱 미다스의 손 (캐릭터) 배찌를 얻었어요!");
+    TALK_PICK_BOOKMARK_1000("작성한 톡픽을 1000명이나 저장! ‘ 킹왕짱 미다스의 손 (캐릭터) 배찌를 얻었어요!"),
+    TALK_PICK_VOTE("작성한 톡픽에 벌써 %d명이 투표했어요!"),
+    TALK_PICK_VOTE_100("작성한 톡픽에 투표한 사람이 100명! ‘따끈한 핫플 (캐릭터) 배찌를 얻었어요!"),
+    TALK_PICK_VOTE_1000("작성한 톡픽에 투표한 사람이 1000명! ‘후끈한 핫플 (캐릭터) 배찌를 얻었어요!");
 
     private final String message;
 
