b91820db46729f9c9352b1cf9827e5a3af684dcc
king_0417 <73704053+gywns0417@users.noreply.github.com>
2024-08-28 00:14:35 +0900

    feat: 댓글 좋아요 알림 메시지 변경

--- src/main/java/balancetalk/global/notification/domain/NotificationMessage.java
+++ src/main/java/balancetalk/global/notification/domain/NotificationMessage.java
@@ -8,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 public enum NotificationMessage {
     FIRST_COMMENT_REPLY("내가 작성한 댓글에 첫 답글이 달렸어요!"),
     COMMENT_REPLY("내가 작성한 댓글에 %d개의 답글이 달렸어요!"),
-    COMMENT_LIKE("내가 작성한 댓글이 %d개의 좋아요를 받았어요!"),
+    COMMENT_LIKE("작성한 댓글이 하트 %d개를 달성했어요!"),
     COMMENT_LIKE_100("작성한 댓글에 하트가 100개! ‘공감왕 (캐릭터) 배찌를 얻었어요!"),
     COMMENT_LIKE_1000("1000개 : 작성한 댓글에 하트가 1000개! ‘공감대왕 (캐릭터) 배찌를 얻었어요!");
 
