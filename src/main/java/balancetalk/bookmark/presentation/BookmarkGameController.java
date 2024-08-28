bd85bce441f99f3b8e41c578eacec0dddcd28681
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2024-08-28 21:15:50 +0900

    style: memberId 필드가 보이지 않도록 수정 (#546)

--- src/main/java/balancetalk/bookmark/presentation/BookmarkGameController.java
+++ src/main/java/balancetalk/bookmark/presentation/BookmarkGameController.java
@@ -4,6 +4,7 @@ import balancetalk.bookmark.application.BookmarkGameService;
 import balancetalk.global.utils.AuthPrincipal;
 import balancetalk.member.dto.ApiMember;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,13 +21,15 @@ public class BookmarkGameController {
 
     @Operation(summary = "밸런스 게임 북마크", description = "밸런스 게임 북마크를 활성화합니다.")
     @PostMapping
-    public void bookmarkGame(@PathVariable final Long gameId, @AuthPrincipal ApiMember apiMember) {
+    public void bookmarkGame(@PathVariable final Long gameId,
+                             @Parameter(hidden = true) @AuthPrincipal ApiMember apiMember) {
         bookmarkGameService.createBookmark(gameId, apiMember);
     }
 
     @Operation(summary = "밸런스 게임 북마크 취소", description = "밸런스 게임 북마크를 취소합니다.")
     @DeleteMapping
-    public void deleteBookmarkGame(@PathVariable final Long gameId, @AuthPrincipal ApiMember apiMember) {
+    public void deleteBookmarkGame(@PathVariable final Long gameId,
+                                   @Parameter(hidden = true) @AuthPrincipal ApiMember apiMember) {
         bookmarkGameService.deleteBookmark(gameId, apiMember);
     }
 }
--- src/main/java/balancetalk/bookmark/presentation/BookmarkTalkPickController.java
+++ src/main/java/balancetalk/bookmark/presentation/BookmarkTalkPickController.java
@@ -4,6 +4,7 @@ import balancetalk.bookmark.application.BookmarkTalkPickService;
 import balancetalk.global.utils.AuthPrincipal;
 import balancetalk.member.dto.ApiMember;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -18,13 +19,15 @@ public class BookmarkTalkPickController {
 
     @Operation(summary = "톡픽 북마크 추가", description = "북마크에 톡픽을 추가합니다.")
     @PostMapping
-    public void createBookmark(@PathVariable Long talkPickId, @AuthPrincipal ApiMember apiMember) {
+    public void createBookmark(@PathVariable Long talkPickId,
+                               @Parameter(hidden = true) @AuthPrincipal ApiMember apiMember) {
         bookmarkTalkPickService.createBookmark(talkPickId, apiMember);
     }
 
     @Operation(summary = "톡픽 북마크 제거", description = "북마크에서 톡픽을 제거합니다.")
     @DeleteMapping
-    public void deleteBookmarkTalkPick(@PathVariable Long talkPickId, @AuthPrincipal ApiMember apiMember) {
+    public void deleteBookmarkTalkPick(@PathVariable Long talkPickId,
+                                       @Parameter(hidden = true) @AuthPrincipal ApiMember apiMember) {
         bookmarkTalkPickService.deleteBookmark(talkPickId, apiMember);
     }
 }
