7b997a341e76e45c695235e6f6e78b9377d16b3d
king_0417 <73704053+gywns0417@users.noreply.github.com>
2024-08-30 23:00:52 +0900

    feat: 게임 알림 생성 요청 DTO 구현

--- src/main/java/balancetalk/global/notification/dto/NotificationDto.java
+++ src/main/java/balancetalk/global/notification/dto/NotificationDto.java
@@ -1,5 +1,6 @@
 package balancetalk.global.notification.dto;
 
+import balancetalk.game.domain.Game;
 import balancetalk.global.notification.domain.Notification;
 import balancetalk.member.domain.Member;
 import balancetalk.talkpick.domain.TalkPick;
@@ -15,7 +16,7 @@ public class NotificationDto {
     @Builder
     @AllArgsConstructor
     @Schema(description = "톡픽 알림 생성 요청")
-    public static class NotificationRequest {
+    public static class TalkPickNotificationRequest {
 
         @Schema(description = "알림 제목", example = "톡픽")
         private String category;
@@ -28,6 +29,29 @@ public class NotificationDto {
                     .member(member)
                     .category(category)
                     .resourceTitle(talkPick.getTitle())
+                    .message(message)
+                    .isRead(false)
+                    .build();
+        }
+    }
+
+    @Data
+    @Builder
+    @AllArgsConstructor
+    @Schema(description = "게임 알림 생성 요청")
+    public static class GameNotificationRequest {
+
+        @Schema(description = "알림 제목", example = "MY 밸런스게임")
+        private String category;
+
+        @Schema(description = "알림 상세 메시지", example = "작성한 댓글이 하트 10개를 달성했어요.")
+        private String message;
+
+        public static Notification toEntity(Member member, Game game, String category, String message) {
+            return Notification.builder()
+                    .member(member)
+                    .category(category)
+                    .resourceTitle(game.getTitle())
                     .message(message)
                     .isRead(false)
                     .build();
