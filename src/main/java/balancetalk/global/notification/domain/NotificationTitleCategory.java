7327881cb683a8ce08501bd89b4b6fac95f01b8d
king_0417 <73704053+gywns0417@users.noreply.github.com>
2024-08-30 22:59:19 +0900

    feat: 알림 제목 카테고리 메시지 변경

--- src/main/java/balancetalk/global/notification/domain/NotificationTitleCategory.java
+++ src/main/java/balancetalk/global/notification/domain/NotificationTitleCategory.java
@@ -7,9 +7,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum NotificationTitleCategory {
 
-    WRITTEN_TALK_PICK("내가 작성한 톡픽"),
+    WRITTEN_TALK_PICK("MY 톡픽"),
     OTHERS_TALK_PICK("톡픽"),
-    WRITTEN_GAME("내가 만든 밸런스게임"),
+    WRITTEN_GAME("MY 밸런스게임"),
     OTHERS_GAME("밸런스게임");
 
     private final String category;
