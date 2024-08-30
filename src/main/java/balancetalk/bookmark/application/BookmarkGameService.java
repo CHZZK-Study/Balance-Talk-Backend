f9958824395180d8febcb484c2adfec37e629dbd
king_0417 <73704053+gywns0417@users.noreply.github.com>
2024-08-30 23:11:54 +0900

    feat: 내가 만든 밸런스 게임 저장 개수 알림 기능 구현

--- src/main/java/balancetalk/bookmark/application/BookmarkGameService.java
+++ src/main/java/balancetalk/bookmark/application/BookmarkGameService.java
@@ -1,6 +1,14 @@
 package balancetalk.bookmark.application;
 
 import static balancetalk.bookmark.domain.BookmarkType.*;
+import static balancetalk.global.notification.domain.NotificationMessage.GAME_BOOKMARK;
+import static balancetalk.global.notification.domain.NotificationMessage.GAME_BOOKMARK_100;
+import static balancetalk.global.notification.domain.NotificationMessage.GAME_BOOKMARK_1000;
+import static balancetalk.global.notification.domain.NotificationStandard.FIRST_STANDARD_OF_NOTIFICATION;
+import static balancetalk.global.notification.domain.NotificationStandard.FOURTH_STANDARD_OF_NOTIFICATION;
+import static balancetalk.global.notification.domain.NotificationStandard.SECOND_STANDARD_OF_NOTIFICATION;
+import static balancetalk.global.notification.domain.NotificationStandard.THIRD_STANDARD_OF_NOTIFICATION;
+import static balancetalk.global.notification.domain.NotificationTitleCategory.WRITTEN_TALK_PICK;
 
 import balancetalk.bookmark.domain.Bookmark;
 import balancetalk.bookmark.domain.BookmarkGenerator;
@@ -9,9 +17,12 @@ import balancetalk.game.domain.Game;
 import balancetalk.game.domain.GameReader;
 import balancetalk.global.exception.BalanceTalkException;
 import balancetalk.global.exception.ErrorCode;
+import balancetalk.global.notification.application.NotificationService;
 import balancetalk.member.domain.Member;
 import balancetalk.member.domain.MemberRepository;
 import balancetalk.member.dto.ApiMember;
+import balancetalk.talkpick.domain.TalkPick;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,6 +36,7 @@ public class BookmarkGameService {
     private final BookmarkRepository bookmarkRepository;
     private final BookmarkGenerator bookmarkGenerator;
     private final MemberRepository memberRepository;
+    private final NotificationService notificationService;
 
     public void createBookmark(final Long gameId, final ApiMember apiMember) {
         Game game = gameReader.readById(gameId);
@@ -56,5 +68,37 @@ public class BookmarkGameService {
         }
         bookmark.deactivate();
         game.decreaseBookmarks();
+        sendBookmarkGameNotification(game);
+    }
+
+    private void sendBookmarkGameNotification(Game game) {
+        Member member = game.getMember();
+        long bookmarkedCount = game.getBookmarks();
+        String bookmarkCountKey = "BOOKMARK_" + bookmarkedCount;
+        Map<String, Boolean> notificationHistory = game.getNotificationHistory();
+        String category = WRITTEN_TALK_PICK.getCategory();
+
+        boolean isMilestoneBookmarked = (bookmarkedCount == FIRST_STANDARD_OF_NOTIFICATION.getCount() ||
+                bookmarkedCount == SECOND_STANDARD_OF_NOTIFICATION.getCount() ||
+                bookmarkedCount == THIRD_STANDARD_OF_NOTIFICATION.getCount() ||
+                (bookmarkedCount > THIRD_STANDARD_OF_NOTIFICATION.getCount() &&
+                        bookmarkedCount % THIRD_STANDARD_OF_NOTIFICATION.getCount() == 0) ||
+                (bookmarkedCount > FOURTH_STANDARD_OF_NOTIFICATION.getCount() &&
+                        bookmarkedCount % FOURTH_STANDARD_OF_NOTIFICATION.getCount() == 0));
+
+        // 북마크 개수가 10, 50, 100*n개, 1000*n개 일 때 알림
+        if (isMilestoneBookmarked && !notificationHistory.getOrDefault(bookmarkCountKey, false)) {
+            notificationService.sendGameNotification(member, game, category, GAME_BOOKMARK.format(bookmarkedCount));
+            // 북마크 개수가 100개일 때 배찌 획득 알림
+            if (bookmarkedCount == THIRD_STANDARD_OF_NOTIFICATION.getCount()) {
+                notificationService.sendGameNotification(member, game, category, GAME_BOOKMARK_100.getMessage());
+            }
+            // 북마크 개수가 1000개일 때 배찌 획득 알림
+            else if (bookmarkedCount == FOURTH_STANDARD_OF_NOTIFICATION.getCount()) {
+                notificationService.sendGameNotification(member, game, category, GAME_BOOKMARK_1000.getMessage());
+            }
+            notificationHistory.put(bookmarkCountKey, true);
+            game.setNotificationHistory(notificationHistory);
+        }
     }
 }
