7947a253e87dcc175bed4922cda123e63091bbc3
king_0417 <73704053+gywns0417@users.noreply.github.com>
2024-08-31 22:16:50 +0900

    feat: SSE 연결 시 미확인 알림 전체 전송 기능, 알림 읽음 처리 기능 구현

--- src/main/java/balancetalk/global/notification/presentation/NotificationController.java
+++ src/main/java/balancetalk/global/notification/presentation/NotificationController.java
@@ -7,6 +7,8 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
@@ -21,5 +23,11 @@ public class NotificationController {
     @Operation(summary = "알림 스트리밍", description = "로그인한 사용자의 알림을 실시간으로 스트리밍합니다.")
     public SseEmitter streamNotifications(@AuthPrincipal ApiMember apiMember) {
         return notificationService.createEmitter(apiMember);
+    }
+
+    @PostMapping("/notifications/{id}/read")
+    @Operation(summary = "알림 읽음 처리", description = "알림을 읽음 상태로 변경합니다.")
+    public void markNotificationAsRead(@PathVariable Long id) {
+        notificationService.markNotificationAsRead(id);
     }
 }
