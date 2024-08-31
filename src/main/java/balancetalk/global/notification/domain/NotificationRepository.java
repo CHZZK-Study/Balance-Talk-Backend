1e320d5ae467be82351de22411ef6c36346be9c6
king_0417 <73704053+gywns0417@users.noreply.github.com>
2024-08-31 22:15:31 +0900

    feat: 읽지 않은 알림 조회 JPA 메서드 작성

--- src/main/java/balancetalk/global/notification/domain/NotificationRepository.java
+++ src/main/java/balancetalk/global/notification/domain/NotificationRepository.java
@@ -1,6 +1,9 @@
 package balancetalk.global.notification.domain;
 
+import balancetalk.member.domain.Member;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findAllByMemberAndReadStatusIsFalseOrderByCreatedAtDesc(Member member);
 }
