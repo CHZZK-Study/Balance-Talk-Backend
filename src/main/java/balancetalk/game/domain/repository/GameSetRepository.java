653645cd3b427a00df1735b6346a2a05871e3010
jschoi-96 <choix712@umn.edu>
2024-09-12 22:17:18 +0900

    feat: GameSet 엔티티 생성 및 레포지토리 추가

---
+++ src/main/java/balancetalk/game/domain/repository/GameSetRepository.java
@@ -0,0 +1,29 @@
+package balancetalk.game.domain.repository;
+
+import balancetalk.game.domain.Game;
+import balancetalk.game.domain.GameSet;
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface GameSetRepository extends JpaRepository<GameSet, Long> {
+
+    @Query("SELECT g FROM GameSet g " +
+            "WHERE g.mainTag.id IN (" +
+            "    SELECT mt.id FROM MainTag mt " +
+            "    WHERE mt.name = :name" +
+            ") " +
+            "ORDER BY g.createdAt DESC")
+    List<Game> findGamesByCreated(@Param("name") String mainTag, Pageable pageable);
+
+    @Query("SELECT g FROM GameSet g " +
+            "WHERE g.mainTag.id IN (" +
+            "    SELECT mt.id FROM MainTag mt " +
+            "    WHERE mt.name = :name" +
+            ") " +
+            "ORDER BY g.views DESC, g.createdAt DESC")
+    List<Game> findGamesByViews(@Param("name") String mainTag, Pageable pageable);
+}
