f8100d94336d833b3b65a29405523a929584aa51
jschoi-96 <choix712@umn.edu>
2024-09-17 00:25:49 +0900

    feat: 밸런스 게임 세트 목록을 응답값으로 주는 dto 생성

--- src/main/java/balancetalk/game/dto/GameSetDto.java
+++ src/main/java/balancetalk/game/dto/GameSetDto.java
@@ -1,5 +1,6 @@
 package balancetalk.game.dto;
 
+import balancetalk.game.domain.Game;
 import balancetalk.game.domain.GameSet;
 import balancetalk.game.domain.MainTag;
 import balancetalk.game.dto.GameDto.CreateGameRequest;
@@ -7,6 +8,8 @@ import balancetalk.game.dto.GameDto.GameDetailResponse;
 import balancetalk.member.domain.Member;
 import balancetalk.vote.domain.VoteOption;
 import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import lombok.AllArgsConstructor;
@@ -32,6 +35,43 @@ public class GameSetDto {
                     .subTag(subTag)
                     .member(member)
                     .games(games.stream().map(CreateGameRequest::toEntity).toList())
+                    .build();
+        }
+    }
+
+    @Data
+    @Builder
+    @AllArgsConstructor
+    @Schema(description = "밸런스 게임 세트 목록 조회 응답")
+    public static class GameSetResponse {
+
+        @Schema(description = "밸런스 게임 세트 id", example = "1")
+        private Long id;
+
+        @Schema(description = "메인 태그", example = "사랑")
+        private String mainTag;
+
+        @Schema(description = "서브 태그", example = "서브 태그")
+        private String subTag;
+
+        @Schema(description = "게시글 제목", example = "제목")
+        private String title;
+
+        private List<String> images;
+
+        public static GameSetResponse fromEntity(GameSet gameSet) {
+            Game game = gameSet.getGames().get(0);
+            List<String> images = new ArrayList<>();
+            images.addAll(Arrays.asList(
+                    game.getGameOptions().get(0).getImgUrl(),
+                    game.getGameOptions().get(1).getImgUrl()
+            ));
+            return GameSetResponse.builder()
+                    .id(gameSet.getId())
+                    .mainTag(gameSet.getMainTag().getName())
+                    .subTag(gameSet.getSubTag())
+                    .title(game.getTitle())
+                    .images(images)
                     .build();
         }
     }
