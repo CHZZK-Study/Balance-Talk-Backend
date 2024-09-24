7292991a3691a05279118c98a2c5c48cf2790303
king_0417 <73704053+gywns0417@users.noreply.github.com>
2024-09-25 01:50:28 +0900

    feat: 답글 조회 JPA 메서드 추가, 댓글 조회 JPA 메서드 수정 서비스 레이어에 적용

--- src/main/java/balancetalk/comment/application/CommentService.java
+++ src/main/java/balancetalk/comment/application/CommentService.java
@@ -102,7 +102,7 @@ public class CommentService {
                                                             GuestOrApiMember guestOrApiMember) {
         validateTalkPickId(talkPickId);
 
-        Page<Comment> comments = commentRepository.findAllByTalkPickId(talkPickId, pageable);
+        Page<Comment> comments = commentRepository.findAllByTalkPickIdAndParentIsNull(talkPickId, pageable);
 
         return comments.map(comment -> {
             int likesCount = likeRepository.countByResourceIdAndLikeType(comment.getId(), LikeType.COMMENT);
@@ -112,11 +112,30 @@ public class CommentService {
     }
 
     @Transactional(readOnly = true)
+    public Page<CommentDto.CommentResponse> findAllReplies(Long parentId, Pageable pageable,
+                                                        GuestOrApiMember guestOrApiMember) {
+        // 부모 댓글이 존재하는지 확인
+        validateCommentId(parentId);
+
+        long memberId = guestOrApiMember.getMemberId();
+
+        // 해당 부모 댓글의 답글 조회
+        Page<Comment> replies = commentRepository.findAllRepliesByParentIdOrderByMemberAndCreatedAt(parentId, memberId, pageable);
+
+        return replies.map(reply -> {
+            int likesCount = likeRepository.countByResourceIdAndLikeType(reply.getId(), LikeType.COMMENT);
+            boolean myLike = isCommentMyLiked(reply.getId(), guestOrApiMember);
+            return CommentDto.CommentResponse.fromEntity(reply, likesCount, myLike);
+        });
+    }
+
+
+    @Transactional(readOnly = true)
     public Page<CommentDto.CommentResponse> findAllBestComments(Long talkPickId, Pageable pageable,
                                                                 GuestOrApiMember guestOrApiMember) {
         validateTalkPickId(talkPickId);
 
-        List<Comment> allComments = commentRepository.findByTalkPickIdOrderByLikesCountDescCreatedAtDesc(talkPickId,
+        List<Comment> allComments = commentRepository.findByTalkPickIdAndParentIsNullOrderByLikesCountDescCreatedAtDesc(talkPickId,
                 LikeType.COMMENT);
         List<CommentDto.CommentResponse> bestComments = new ArrayList<>();
         List<CommentDto.CommentResponse> otherComments = new ArrayList<>();
