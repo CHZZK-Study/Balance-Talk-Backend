7567256d286c4dbb511262870a5ba6ab4ebd2677
king_0417 <73704053+gywns0417@users.noreply.github.com>
2024-09-25 04:17:37 +0900

    feat: 밸런스게임 검색 쿼리 작성

--- src/main/java/balancetalk/game/domain/repository/GameRepository.java
+++ src/main/java/balancetalk/game/domain/repository/GameRepository.java
@@ -11,5 +11,29 @@ import org.springframework.data.repository.query.Param;
 
 public interface GameRepository extends JpaRepository<Game, Long> {
 
-//    Page<Game> findAllByMemberIdOrderByEditedAtDesc(Long memberId, Pageable pageable);
+    //    Page<Game> findAllByMemberIdOrderByEditedAtDesc(Long memberId, Pageable pageable);
+
+    @Query(value = "SELECT DISTINCT g.* " +
+            "FROM game g " +
+            "LEFT JOIN game_set gs ON g.game_set_id = gs.id " +
+            "LEFT JOIN game_option go ON go.game_id = g.id " +
+            "WHERE " +
+            "gs.sub_tag = :query OR " +
+            "g.title = :query OR " +
+            "g.description = :query OR " +
+            "go.name = :query OR " +
+            "go.description = :query",
+            nativeQuery = true)
+    List<Game> searchExactMatch(@Param("query") String query);
+
+    @Query(value = "SELECT DISTINCT g.* " +
+            "FROM game g " +
+            "INNER JOIN game_set gs ON g.game_set_id = gs.id " +
+            "INNER JOIN game_option go ON go.game_id = g.id " +
+            "WHERE " +
+            "MATCH(gs.sub_tag) AGAINST (:query IN NATURAL LANGUAGE MODE) OR " +
+            "MATCH(g.title, g.description) AGAINST (:query IN NATURAL LANGUAGE MODE) OR " +
+            "MATCH(go.name, go.description) AGAINST (:query IN NATURAL LANGUAGE MODE)",
+            nativeQuery = true)
+    List<Game> searchNaturalLanguage(@Param("query") String query);
 }
