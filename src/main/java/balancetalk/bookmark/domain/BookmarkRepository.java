84725a3bd70a77613db11c303ab0e5de9150706c
king_0417 <73704053+gywns0417@users.noreply.github.com>
2024-09-27 19:35:42 +0900

    feat: 밸런스게임 세트 전체 투표 완료 여부를 알기 위한 JPA 메서드 작성

--- src/main/java/balancetalk/bookmark/domain/BookmarkRepository.java
+++ src/main/java/balancetalk/bookmark/domain/BookmarkRepository.java
@@ -1,18 +1,19 @@
 package balancetalk.bookmark.domain;
 
 import balancetalk.member.domain.Member;
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
-
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
 
     @Query("SELECT b FROM Bookmark b WHERE b.member = :member AND b.bookmarkType = :bookmarkType AND b.active = true ORDER BY b.lastModifiedAt DESC")
     Page<Bookmark> findActivatedByMemberOrderByDesc(@Param("member") Member member, @Param("bookmarkType") BookmarkType bookmarkType,
                                                     Pageable pageable);
+
+    Optional<Bookmark> findByMemberAndResourceIdAndBookmarkType(Member member, Long resourceId, BookmarkType bookmarkType);
 
 }
