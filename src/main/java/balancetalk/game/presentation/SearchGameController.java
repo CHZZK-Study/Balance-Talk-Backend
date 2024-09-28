751a3022cd43dc6e8a11503c760f2cf9acbce49d
king_0417 <73704053+gywns0417@users.noreply.github.com>
2024-09-28 22:01:03 +0900

    style: 밸런스게임 검색 Swagger 추가

--- src/main/java/balancetalk/game/presentation/SearchGameController.java
+++ src/main/java/balancetalk/game/presentation/SearchGameController.java
@@ -2,6 +2,8 @@ package balancetalk.game.presentation;
 
 import balancetalk.game.application.SearchGameService;
 import balancetalk.game.dto.SearchGameResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -12,10 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "search", description = "밸런스게임 검색 API")
 public class SearchGameController {
 
     private final SearchGameService searchGameService;
 
+    @Operation(summary = "밸런스게임 검색", description = "밸런스게임을 검색합니다. (정렬 기준 : views, createdAt)")
     @GetMapping("/search/game")
     public Page<SearchGameResponse> searchTalkPicks(@RequestParam("query") String query,
                                                     @RequestParam(defaultValue = "0") int page,
