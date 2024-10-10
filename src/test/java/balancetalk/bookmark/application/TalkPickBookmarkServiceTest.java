2cae10a63c6fdad3e03765da01509a9b51ffc3f1
king_0417 <73704053+gywns0417@users.noreply.github.com>
2024-10-10 21:52:37 +0900

    style: 테스트 클래스 네이밍 변경

--- src/test/java/balancetalk/bookmark/application/TalkPickBookmarkServiceTest.java
+++ src/test/java/balancetalk/bookmark/application/TalkPickBookmarkServiceTest.java
@@ -24,7 +24,7 @@ import java.util.Optional;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class BookmarkTalkPickTalkPickServiceTest {
+class TalkPickBookmarkServiceTest {
 
     @InjectMocks
     BookmarkTalkPickService bookmarkTalkPickService;
