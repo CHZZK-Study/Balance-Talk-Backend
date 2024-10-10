d7d268a7b48abfbc1309f1c2777f6b18031c900f
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-10-10 21:30:32 +0900

    AWS SDK for Java를 Spring Cloud로 마이그레이션 (#623)
    * chore: AWS SDK for Java 의존성 제거 및 Spring Cloud 의존성 추가
    
    * refactor: 이미지 업로드 기능 Spring Cloud로 마이그레이션
    
    * refactor: 이미지 제거 기능 Spring Cloud로 마이그레이션
    
    * refactor: 이미지 관련 기능을 Spring Cloud로 마이그레이션하면서 필요 없어진 클래스 제거
    
    * style: 변수명 맨앞 문자를 소문자로 변경
    
    * refactor: 사용하지 않는 클래스 파일 제거
    
    * refactor: Spring Cloud로 마이그레이션하면서 더 이상 사용되지 않는 클래스의 테스트 파일 제거
    
    * style: 불필요한 로그 출력 코드 제거

--- src/main/java/balancetalk/file/application/FileService.java
+++ src/main/java/balancetalk/file/application/FileService.java
@@ -5,18 +5,16 @@ import balancetalk.file.domain.FileProcessor;
 import balancetalk.file.domain.FileType;
 import balancetalk.file.domain.MultipartFiles;
 import balancetalk.file.domain.repository.FileRepository;
-import balancetalk.file.domain.s3.S3ImageRemover;
-import balancetalk.file.domain.s3.S3ImageUploader;
-import balancetalk.file.domain.s3.S3ImageUrlGetter;
 import balancetalk.file.dto.UploadFileResponse;
 import balancetalk.global.exception.BalanceTalkException;
+import io.awspring.cloud.s3.S3Operations;
+import io.awspring.cloud.s3.S3Resource;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
-import software.amazon.awssdk.services.s3.S3Client;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,17 +27,14 @@ import static balancetalk.global.exception.ErrorCode.NOT_UPLOADED_IMAGE_FOR_DB_E
 @RequiredArgsConstructor
 public class FileService {
 
-    private final S3Client s3Client;
+    private final S3Operations s3Operations;
     private final FileProcessor fileProcessor;
-    private final S3ImageUploader s3ImageUploader;
-    private final S3ImageRemover s3ImageRemover;
-    private final S3ImageUrlGetter s3ImageUrlGetter;
     private final FileRepository fileRepository;
 
-    @Value(value = "${aws.s3.end-point}")
+    @Value(value = "${picko.aws.s3.endpoint}")
     private String s3EndPoint;
 
-    @Value("${aws.s3.bucket}")
+    @Value("${spring.cloud.aws.s3.bucket}")
     private String bucket;
 
     @Transactional
@@ -47,6 +42,7 @@ public class FileService {
         FileType fileType = multipartFiles.fileType();
 
         List<String> s3Keys = new ArrayList<>();
+        List<String> imgUrls = new ArrayList<>();
         List<String> storedNames = new ArrayList<>();
         for (MultipartFile multipartFile : multipartFiles.multipartFiles()) {
             try {
@@ -54,7 +50,8 @@ public class FileService {
                 String s3Key = fileType.getUploadDir() + file.getStoredName();
 
                 // S3에 이미지 파일 업로드
-                s3ImageUploader.uploadImageToBucket(s3Client, bucket, s3Key, multipartFile);
+                S3Resource uploaded = s3Operations.upload(bucket, s3Key, multipartFile.getInputStream());
+                imgUrls.add(uploaded.getURL().toString());
 
                 // DB에 메타 데이터 저장
                 fileRepository.save(file);
@@ -62,19 +59,17 @@ public class FileService {
                 // 이미지 URL 및 고유 이름 저장
                 s3Keys.add(s3Key);
                 storedNames.add(file.getStoredName());
-            } catch (Exception e) {
+            } catch (Exception e) { // TODO 예외 처리 구체화
+                log.error(e.getMessage());
+
                 // DB에 메타 데이터 저장 중 예외가 발생하면 S3에 업로드된 이미지 제거 후 커스텀 예외로 변환
                 for (String key : s3Keys) {
-                    s3ImageRemover.removeImageFromBucket(s3Client, bucket, key);
+                    s3Operations.deleteObject(bucket, key);
                 }
 
                 throw new BalanceTalkException(NOT_UPLOADED_IMAGE_FOR_DB_ERROR);
             }
         }
-
-        List<String> imgUrls = s3Keys.stream()
-                .map(key -> s3ImageUrlGetter.getImageUrl(s3Client, bucket, key))
-                .toList();
 
         return new UploadFileResponse(imgUrls, storedNames);
     }
@@ -84,6 +79,6 @@ public class FileService {
         File file = fileRepository.findByStoredName(storedName)
                 .orElseThrow(() -> new BalanceTalkException(NOT_FOUND_FILE));
         fileRepository.delete(file);
-        s3ImageRemover.removeImageFromBucket(s3Client, bucket, file.getS3Key());
+        s3Operations.deleteObject(bucket, file.getS3Key());
     }
 }
