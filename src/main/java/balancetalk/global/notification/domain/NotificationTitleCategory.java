7bc2853b18e1e8ac1a8b5653f643f03316122633
king_0417 <73704053+gywns0417@users.noreply.github.com>
2024-10-15 02:36:23 +0900

    feat: 추후 타인 밸런스게임 세트에 대한 알림 발생 시, 밸런스게임 메인태그 불러오도록 수정

--- src/main/java/balancetalk/global/notification/domain/NotificationTitleCategory.java
+++ src/main/java/balancetalk/global/notification/domain/NotificationTitleCategory.java
@@ -1,5 +1,6 @@
 package balancetalk.global.notification.domain;
 
+import balancetalk.game.domain.GameSet;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -10,12 +11,20 @@ public enum NotificationTitleCategory {
     WRITTEN_TALK_PICK("MY 톡픽"),
     OTHERS_TALK_PICK("톡픽"),
     WRITTEN_GAME("MY 밸런스게임"),
-    OTHERS_GAME("밸런스게임"),
+    OTHERS_GAME(null),
     MY_PICK("MY PICK");
 
     private final String category;
 
     public String format(Object... args) {
-        return String.format(category, args);
+        return String.format(getCategory(), args);
+    }
+
+    public String getCategory(GameSet gameSet) {
+        if (this == OTHERS_GAME) {
+            // 동적으로 GameSet의 mainTag를 이용해 값을 설정
+            return gameSet.getMainTag().getName(); // GameSet 인스턴스를 외부에서 제공받아 사용
+        }
+        return category;
     }
 }
