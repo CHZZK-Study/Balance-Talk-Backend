7d69b3efd1207e71a2435cc13542c925ca451716
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-10-29 20:30:43 +0900

    style: 줄바꿈을 통해 가독성 개선

--- src/main/java/balancetalk/file/presentation/FileController.java
+++ src/main/java/balancetalk/file/presentation/FileController.java
@@ -7,12 +7,17 @@ import balancetalk.file.dto.UploadFileResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,7 +30,8 @@ public class FileController {
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     @Operation(summary = "이미지 파일 업로드", description = "이미지 파일을 업로드한 후 이미지 URL을 반환 받습니다.")
     public UploadFileResponse uploadImage(@RequestPart("file") List<MultipartFile> multipartFiles,
-                                          @Parameter(description = "리소스 타입", example = "TALK_PICK") @RequestParam("type") FileType fileType) {
+                                          @Parameter(description = "리소스 타입", example = "TALK_PICK")
+                                          @RequestParam("type") FileType fileType) {
         return fileService.uploadImages(new MultipartFiles(multipartFiles, fileType));
     }
 
