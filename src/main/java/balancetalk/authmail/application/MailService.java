6be2a0883f5991260d8312cced41291775ff620b
jschoi-96 <choix712@umn.edu>
2024-10-31 00:59:07 +0900

    feat: 비밀번호 재설정 api 생성

--- src/main/java/balancetalk/authmail/application/MailService.java
+++ src/main/java/balancetalk/authmail/application/MailService.java
@@ -1,7 +1,9 @@
 package balancetalk.authmail.application;
 
-import balancetalk.authmail.dto.EmailRequest;
-import balancetalk.authmail.dto.EmailVerification;
+import static balancetalk.authmail.dto.EmailDto.*;
+import static jakarta.mail.Message.RecipientType.TO;
+
+import balancetalk.authmail.dto.EmailDto.EmailRequest;
 import balancetalk.global.caffeine.CacheType;
 import balancetalk.global.exception.BalanceTalkException;
 import balancetalk.global.exception.ErrorCode;
@@ -10,23 +12,16 @@ import balancetalk.member.domain.MemberRepository;
 import com.github.benmanes.caffeine.cache.Cache;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.caffeine.CaffeineCache;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.security.SecureRandom;
-import java.util.Optional;
 import java.util.UUID;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional
@@ -35,109 +30,71 @@ public class MailService {
     private static final String SENDER_EMAIL = "bootsprng@gmail.com";
     private static final int AUTH_NUMBER_MIN = 0;
     private static final int AUTH_NUMBER_MAX = 6;
-    private static final int RANDOM_GENERATOR_MIN = 33;
-    private static final int RANDOM_GENERATOR_MAX = 126;
-    private static final int RANDOM_GENERATOR_LENGTH = 10;
 
     private final JavaMailSender javaMailSender;
     private final CacheManager cacheManager;
     private final MemberRepository memberRepository;
-    private final PasswordEncoder passwordEncoder;
+    private final BCryptPasswordEncoder passwordEncoder;
 
-    @Value("${spring.mail.auth-code-expiration-millis}")
-    private long authCodeExpirationMillis;
-
-    public void sendAuthenticationNumber(EmailRequest request){
-        Optional<Member> member = memberRepository.findByEmail(request.getEmail());
-        if (member.isPresent()) {
-            throw new BalanceTalkException(ErrorCode.ALREADY_REGISTERED_EMAIL);
-        }
+    public void sendAuthenticationNumber(EmailRequest request) {
         MimeMessage message = createTempCode(request);
         javaMailSender.send(message);
     }
 
-    public void verifyCode(EmailVerification request) {
-        Optional<Member> member = memberRepository.findByEmail(request.getEmail());
-        if (member.isPresent()) {
-            throw new BalanceTalkException(ErrorCode.ALREADY_REGISTERED_EMAIL);
-        }
-
-        CaffeineCache cache = (CaffeineCache) cacheManager.getCache(CacheType.TempCode.getCacheName());
-        Cache<Object, Object> nativeCache = cache.getNativeCache();
-        Object value = nativeCache.getIfPresent(request.getEmail());
-
-        if (!request.getVerificationCode().equals(value)) {
-            throw new BalanceTalkException(ErrorCode.AUTHORIZATION_CODE_MISMATCH);
-        }
+    private String createNumber() {
+        return UUID.randomUUID().toString().substring(AUTH_NUMBER_MIN, AUTH_NUMBER_MAX);
     }
 
-    public void sendTempPassword(EmailRequest request) {
-        MimeMessage message = createTempPassword(request);
-        javaMailSender.send(message);
-    }
-
-    private MimeMessage createTempCode(EmailRequest request){
+    private MimeMessage createTempCode(EmailRequest request) {
         String authCode = createNumber();
         MimeMessage message = javaMailSender.createMimeMessage();
         try {
             message.setFrom(SENDER_EMAIL);
-            message.setRecipients(MimeMessage.RecipientType.TO, request.getEmail());
+            message.setRecipients(TO, request.getEmail());
             message.setSubject("[BalanceTalk] 인증 번호");
             String body = "";
             body += "<h3>" + "요청하신 인증 번호입니다." + "</h3>";
             body += "<h1>" + authCode + "</h1>";
             body += "<h3>" + "감사합니다." + "</h3>";
-            message.setText(body,"UTF-8", "html");
+            message.setText(body, "UTF-8", "html");
         } catch (MessagingException e) {
             throw new BalanceTalkException(ErrorCode.FAIL_SEND_EMAIL);
         }
 
-        cacheManager.getCache(CacheType.TempCode.getCacheName()).put(request.getEmail(), authCode);
+        Optional.ofNullable(cacheManager.getCache(CacheType.TempCode.getCacheName()))
+                .ifPresentOrElse(
+                        cache -> cache.put(request.getEmail(), authCode),
+                        () -> {
+                            throw new BalanceTalkException(ErrorCode.CACHE_NOT_FOUND);
+                        }
+                );
         return message;
     }
 
-    private MimeMessage createTempPassword(EmailRequest request) {
-        String tempPwd = createTempPassword();
-        MimeMessage message = javaMailSender.createMimeMessage();
-        try {
-            message.setFrom(SENDER_EMAIL);
-            message.setRecipients(MimeMessage.RecipientType.TO, request.getEmail());
-            message.setSubject("[BalanceTalk] 임시 비밀번호");
+    public void verifyCode(EmailVerification request) {
+        CaffeineCache cache = (CaffeineCache) cacheManager.getCache(CacheType.TempCode.getCacheName());
+        Cache<Object, Object> nativeCache = Optional.ofNullable(cache)
+                .map(CaffeineCache::getNativeCache)
+                .orElseThrow(() -> new BalanceTalkException(ErrorCode.CACHE_NOT_FOUND));
+        Object value = nativeCache.getIfPresent(request.getEmail());
 
-            String body = "";
-            body += "<h3>" + "임시 비밀번호 입니다." + "</h3>";
-            body += "<h1>" + tempPwd + "</h1>";
-            body += "<h3>" + "로그인 후에 새로운 비밀번호로 변경하셔야 합니다." + "</h3>";
-            body += "<h3>" + "감사합니다." + "</h3>";
-            message.setText(body,"UTF-8", "html");
-            updateTempPassword(tempPwd, request);
-        } catch (MessagingException e) {
-            throw new BalanceTalkException(ErrorCode.FAIL_SEND_EMAIL);
+        String verificationCode = request.getVerificationCode();
+        if (!verificationCode.equals(value)) {
+            throw new BalanceTalkException(ErrorCode.AUTHORIZATION_CODE_MISMATCH);
         }
-        return message;
     }
 
-    public void updateTempPassword(String tempPwd, EmailRequest request) {
-        String encodedTempPwd = passwordEncoder.encode(tempPwd);
-        Member member = memberRepository.findByEmail(request.getEmail())
+    public void resetPassword(PasswordResetRequest request) {
+        String password = request.getPassword();
+        String passwordConfirm = request.getPasswordConfirm();
+        if (!password.equals(passwordConfirm)) {
+            throw new BalanceTalkException(ErrorCode.PASSWORD_MISMATCH);
+        }
+
+        String email = request.getEmail();
+        Member member = memberRepository.findByEmail(email)
                 .orElseThrow(() -> new BalanceTalkException(ErrorCode.NOT_FOUND_MEMBER));
-        member.updatePassword(encodedTempPwd);
-    }
 
-    private String createNumber(){
-        return UUID.randomUUID().toString().substring(AUTH_NUMBER_MIN, AUTH_NUMBER_MAX);
-    }
-
-    private String createTempPassword() {
-        SecureRandom secureRandom = new SecureRandom();
-        String charNSpecialChar = IntStream.rangeClosed(RANDOM_GENERATOR_MIN, RANDOM_GENERATOR_MAX)
-                .mapToObj(i -> String.valueOf((char) i))
-                .collect(Collectors.joining());
-        StringBuilder builder = new StringBuilder();
-        for(int i = 0; i < RANDOM_GENERATOR_LENGTH; i++) {
-            builder.append(charNSpecialChar.charAt(secureRandom.nextInt(charNSpecialChar.length())));
-
-        }
-        return builder.toString();
+        member.updatePassword(passwordEncoder.encode(password));
     }
 }
