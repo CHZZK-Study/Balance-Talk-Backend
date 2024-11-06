de377855237f4bffcebd11ff6d1158de096509f4
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-11-07 02:39:37 +0900

    톡픽 임시 저장 시 모든 필드에 대한 공백 허용 (#695)
    * feat: TempTalkPick의 모든 필드를 공백 허용하도록 수정
    
    * refactor: DTO 필드 검증 로직 분리
    
    * refactor: override 명시

--- src/main/java/balancetalk/talkpick/dto/BaseTalkPickFields.java
+++ src/main/java/balancetalk/talkpick/dto/BaseTalkPickFields.java
@@ -1,7 +1,6 @@
 package balancetalk.talkpick.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,21 +12,17 @@ import lombok.Getter;
 public class BaseTalkPickFields {
 
     @Schema(description = "제목", example = "제목")
-    @NotBlank(message = "제목은 공백을 허용하지 않습니다.")
     @Size(max = 50, message = "제목은 50자 이하여야 합니다.")
     private String title;
 
     @Schema(description = "본문 내용", example = "본문 내용")
-    @NotBlank(message = "본문 내용은 공백을 허용하지 않습니다.")
     private String content;
 
     @Schema(description = "선택지 A 이름", example = "선택지 A 이름")
-    @NotBlank(message = "선택지 이름은 공백을 허용하지 않습니다.")
     @Size(max = 10, message = "선택지 이름은 10자 이하여야 합니다.")
     private String optionA;
 
     @Schema(description = "선택지 B 이름", example = "선택지 B 이름")
-    @NotBlank(message = "선택지 이름은 공백을 허용하지 않습니다.")
     @Size(max = 10, message = "선택지 이름은 10자 이하여야 합니다.")
     private String optionB;
 
---
+++ src/main/java/balancetalk/talkpick/dto/BaseTempTalkPickFields.java
@@ -0,0 +1,34 @@
+package balancetalk.talkpick.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class BaseTempTalkPickFields extends BaseTalkPickFields {
+
+    public BaseTempTalkPickFields(String title, String content, String optionA, String optionB, String sourceUrl) {
+        super(title, content, optionA, optionB, sourceUrl);
+    }
+
+    @NotBlank(message = "제목은 공백을 허용하지 않습니다.")
+    @Override
+    public String getTitle() {
+        return super.getTitle();
+    }
+
+    @NotBlank(message = "본문 내용은 공백을 허용하지 않습니다.")
+    @Override
+    public String getContent() {
+        return super.getContent();
+    }
+
+    @NotBlank(message = "선택지 이름은 공백을 허용하지 않습니다.")
+    @Override
+    public String getOptionA() {
+        return super.getOptionA();
+    }
+
+    @NotBlank(message = "선택지 이름은 공백을 허용하지 않습니다.")
+    @Override
+    public String getOptionB() {
+        return super.getOptionA();
+    }
+}
