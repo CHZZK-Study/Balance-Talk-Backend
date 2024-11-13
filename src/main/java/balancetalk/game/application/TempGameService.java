60125e0cc68890a74bd5be3e5b3eee64abf2038c
jschoi-96 <choix712@umn.edu>
2024-11-14 01:44:28 +0900

    feat: 임시 저장 기능 구현

--- src/main/java/balancetalk/game/application/TempGameService.java
+++ src/main/java/balancetalk/game/application/TempGameService.java
@@ -1,16 +1,17 @@
 package balancetalk.game.application;
 
-import static balancetalk.file.domain.FileType.TEMP_GAME;
+import static balancetalk.file.domain.FileType.TEMP_GAME_OPTION;
 import static balancetalk.game.dto.TempGameDto.CreateTempGameRequest;
 
-import balancetalk.file.domain.File;
 import balancetalk.file.domain.FileHandler;
 import balancetalk.file.domain.repository.FileRepository;
 import balancetalk.game.domain.MainTag;
 import balancetalk.game.domain.TempGame;
+import balancetalk.game.domain.TempGameOption;
 import balancetalk.game.domain.TempGameSet;
 import balancetalk.game.domain.repository.MainTagRepository;
 import balancetalk.game.domain.repository.TempGameSetRepository;
+import balancetalk.game.dto.TempGameOptionDto.CreateTempGameOption;
 import balancetalk.game.dto.TempGameSetDto.CreateTempGameSetRequest;
 import balancetalk.game.dto.TempGameSetDto.TempGameSetResponse;
 import balancetalk.global.exception.BalanceTalkException;
@@ -18,7 +19,7 @@ import balancetalk.global.exception.ErrorCode;
 import balancetalk.member.domain.Member;
 import balancetalk.member.domain.MemberRepository;
 import balancetalk.member.dto.ApiMember;
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -27,6 +28,9 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class TempGameService {
+
+    private static final int GAME_SIZE = 10;
+
     private final MemberRepository memberRepository;
     private final TempGameSetRepository tempGameSetRepository;
     private final FileRepository fileRepository;
@@ -36,8 +40,48 @@ public class TempGameService {
     @Transactional
     public void createTempGame(CreateTempGameSetRequest request, ApiMember apiMember) {
         Member member = apiMember.toMember(memberRepository);
-        if (member.hasTempGameSet()) {
+        MainTag mainTag = mainTagRepository.findByName(request.getMainTag())
+                .orElseThrow(() -> new BalanceTalkException(ErrorCode.NOT_FOUND_MAIN_TAG));
 
+        List<CreateTempGameRequest> tempGames = request.getTempGames();
+        if (tempGames.size() < GAME_SIZE) {
+            throw new BalanceTalkException(ErrorCode.BALANCE_GAME_SIZE_TEN);
+        }
+
+        List<TempGame> newTempGames = tempGames.stream()
+                .map(game -> game.toEntity(fileRepository))
+                .toList();
+
+        if (member.hasTempGameSet()) { // 기존 임시저장이 존재하는 경우
+            TempGameSet existGame = member.getTempGameSet();
+            existGame.updateTempGameSet(request.getTitle(), newTempGames);
+        }
+
+        else {
+            TempGameSet tempGameSet = request.toEntity(request.getTitle(), mainTag, member);
+            tempGameSetRepository.save(tempGameSet);
+        }
+        processTempGameFiles(tempGames, newTempGames);
+    }
+
+    private void processTempGameFiles(List<CreateTempGameRequest> requests, List<TempGame> tempGames) {
+        for (int i = 0; i < requests.size(); i++) {
+            CreateTempGameRequest gameRequest = requests.get(i);
+            TempGame tempGame = tempGames.get(i);
+
+            for (int j = 0; j < gameRequest.getTempGameOptions().size(); j++) {
+                CreateTempGameOption gameOptionDto = gameRequest.getTempGameOptions().get(j);
+                TempGameOption tempGameOption = tempGame.getTempGameOptions().get(j);
+
+                if (gameOptionDto.getFileId() == null) {
+                    continue;
+                }
+
+                fileRepository.findById(gameOptionDto.getFileId())
+                        .ifPresent(
+                                file -> fileHandler.relocateFiles(Collections.singletonList(file), tempGameOption.getId(),
+                                        TEMP_GAME_OPTION));
+            }
         }
     }
 
