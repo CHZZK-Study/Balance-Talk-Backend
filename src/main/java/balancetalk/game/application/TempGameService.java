fedc7510d5011c0a31f6d70f76bf772ece348993
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2024-11-25 22:38:13 +0900

    밸런스 게임 임시 저장 기능 구현 (#713)
    * refactor: 변경된 ERD 구조에 맞춰 필드값 재조정
    
    * refactor: TEMP_GAME -> TEMP_GAME_OPTION으로 파일 타입 변경
    
    * refactor: 사용하지 않는 연관 관계 메서드 제거
    
    * refactor: dto, 연관 관계 메서드 수정
    
    * feat: 임시 저장 기능 구현
    
    * refactor: 임시 저장 조회 메서드 수정
    
    * refactor: 임시 저장 temp_game_option에 파일 정보를 저장하지 않도록 수정
    
    * style: sonarqube
    
    * fix: 파일이 순서대로 저장되게끔 구현
    
    * style: sonarqube
    
    * refactor: game의 갯수 체크를 서비스단에서 dto단으로 이동
    
    * refactor: resourceId와 fileType을 이용하여 파일을 식별하도록 수정
    
    * refactor: 불필요한 필드 제거
    
    * style: sonarqube
    
    * refactor: 에러코드 삭제 및 fileRepository 메서드 네이밍 변경

--- src/main/java/balancetalk/game/application/TempGameService.java
+++ src/main/java/balancetalk/game/application/TempGameService.java
@@ -1,16 +1,17 @@
 package balancetalk.game.application;
 
-import static balancetalk.file.domain.FileType.TEMP_GAME;
+import static balancetalk.file.domain.FileType.TEMP_GAME_OPTION;
 import static balancetalk.game.dto.TempGameDto.CreateTempGameRequest;
 
-import balancetalk.file.domain.File;
 import balancetalk.file.domain.FileHandler;
 import balancetalk.file.domain.repository.FileRepository;
 import balancetalk.game.domain.MainTag;
 import balancetalk.game.domain.TempGame;
+import balancetalk.game.domain.TempGameOption;
 import balancetalk.game.domain.TempGameSet;
 import balancetalk.game.domain.repository.MainTagRepository;
 import balancetalk.game.domain.repository.TempGameSetRepository;
+import balancetalk.game.dto.TempGameOptionDto;
 import balancetalk.game.dto.TempGameSetDto.CreateTempGameSetRequest;
 import balancetalk.game.dto.TempGameSetDto.TempGameSetResponse;
 import balancetalk.global.exception.BalanceTalkException;
@@ -19,6 +20,7 @@ import balancetalk.member.domain.Member;
 import balancetalk.member.domain.MemberRepository;
 import balancetalk.member.dto.ApiMember;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -27,6 +29,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class TempGameService {
+
     private final MemberRepository memberRepository;
     private final TempGameSetRepository tempGameSetRepository;
     private final FileRepository fileRepository;
@@ -39,41 +42,60 @@ public class TempGameService {
         MainTag mainTag = mainTagRepository.findByName(request.getMainTag())
                 .orElseThrow(() -> new BalanceTalkException(ErrorCode.NOT_FOUND_MAIN_TAG));
 
-        if (member.hasTempGameSet()) {
-            TempGameSet tempGameSet = member.updateTempGameSet(request.toEntity(mainTag, member));
-            List<CreateTempGameRequest> tempGameRequests = request.getTempGames();
-            List<TempGame> tempGames = tempGameSet.getTempGames();
+        List<CreateTempGameRequest> tempGames = request.getTempGames();
 
-            for (int i = 0; i < tempGameRequests.size(); i++) {
-                CreateTempGameRequest tempGameRequest = tempGameRequests.get(i);
-                List<File> files = fileRepository.findAllById(tempGameRequest.getFileIds());
+        List<TempGame> newTempGames = tempGames.stream()
+                .map(game -> game.toEntity(fileRepository))
+                .toList();
 
-                TempGame tempGame = tempGames.get(i);
-                fileHandler.relocateFiles(files, tempGame.getId(), TEMP_GAME);
-            }
+        if (member.hasTempGameSet()) { // 기존 임시저장이 존재하는 경우
+            TempGameSet existGame = member.getTempGameSet();
+            existGame.updateTempGameSet(request.getTitle(), newTempGames);
+            processTempGameFiles(tempGames, existGame.getTempGames());
             return;
         }
 
         TempGameSet tempGameSet = request.toEntity(mainTag, member);
-        List<CreateTempGameRequest> tempGameRequests = request.getTempGames();
-        List<TempGame> tempGames = new ArrayList<>();
+        List<TempGame> games = new ArrayList<>();
 
-        for (CreateTempGameRequest tempGameRequest : tempGameRequests) {
-            TempGame tempGame = tempGameRequest.toEntity();
-            tempGames.add(tempGame);
-            List<File> files = fileRepository.findAllById(tempGameRequest.getFileIds());
-            fileHandler.relocateFiles(files, tempGame.getId(), TEMP_GAME);
+        for (CreateTempGameRequest tempGame : tempGames) {
+            TempGame game = tempGame.toEntity(fileRepository);
+            games.add(game);
         }
-
-        tempGameSet.addTempGames(tempGames);
+        tempGameSet.addGames(games);
         tempGameSetRepository.save(tempGameSet);
+        processTempGameFiles(tempGames, tempGameSet.getTempGames());
     }
 
+    private void processTempGameFiles(List<CreateTempGameRequest> requests, List<TempGame> tempGames) {
+
+        for (int i = 0; i < requests.size(); i++) {
+            CreateTempGameRequest gameRequest = requests.get(i);
+            List<TempGameOptionDto> tempGameOptions = gameRequest.getTempGameOptions();
+            TempGame tempGame = tempGames.get(i);
+
+            List<TempGameOption> gameOptions = tempGame.getTempGameOptions();
+
+            for (int j = 0; j < tempGameOptions.size(); j++) {
+                TempGameOptionDto tempGameOptionDto = tempGameOptions.get(j);
+                TempGameOption tempGameOption = gameOptions.get(j);
+
+                Long fileId = tempGameOptionDto.getFileId();
+                Long gameOptionId = tempGameOption.getId();
+                if (fileId != null) {
+                    fileRepository.findById(fileId)
+                        .ifPresent(file -> fileHandler.relocateFiles(Collections.singletonList(file),
+                               gameOptionId, TEMP_GAME_OPTION));
+                }
+            }
+        }
+    }
+
+    @Transactional(readOnly = true)
     public TempGameSetResponse findTempGameSet(ApiMember apiMember) {
         Member member = apiMember.toMember(memberRepository);
         TempGameSet tempGameSet = tempGameSetRepository.findByMember(member)
                 .orElseThrow(() -> new BalanceTalkException(ErrorCode.NOT_FOUND_BALANCE_GAME_SET));
-
-        return TempGameSetResponse.fromEntity(tempGameSet);
+        return TempGameSetResponse.fromEntity(tempGameSet, fileRepository);
     }
 }
--- src/main/java/balancetalk/game/dto/TempGameSetDto.java
+++ src/main/java/balancetalk/game/dto/TempGameSetDto.java
@@ -1,33 +1,48 @@
 package balancetalk.game.dto;
 
+import balancetalk.file.domain.repository.FileRepository;
 import balancetalk.game.domain.MainTag;
 import balancetalk.game.domain.TempGameSet;
 import balancetalk.game.dto.TempGameDto.CreateTempGameRequest;
-import balancetalk.game.dto.TempGameDto.TempGameDetailResponse;
+import balancetalk.game.dto.TempGameDto.TempGameResponse;
+import balancetalk.global.exception.BalanceTalkException;
+import balancetalk.global.exception.ErrorCode;
 import balancetalk.member.domain.Member;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Size;
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 public class TempGameSetDto {
 
+    private static final int GAME_SIZE = 10;
+
     @Data
     public static class CreateTempGameSetRequest {
+
+        @Schema(description = "밸런스게임 세트 제목", example = "밸런스게임 세트 제목")
+        @Size(max = 50, message = "제목은 최대 50자까지 입력 가능합니다")
+        private String title;
 
         @Schema(description = "밸런스 게임 메인 태그", example = "커플")
         private String mainTag;
 
         @Schema(description = "밸런스 게임 서브 태그", example = "커플지옥")
+        @Size(max = 10, message = "서브 태그는 최대 10자까지 입력 가능합니다")
         private String subTag;
 
         private List<CreateTempGameRequest> tempGames;
 
         public TempGameSet toEntity(MainTag mainTag, Member member) {
+            if (tempGames == null || tempGames.size() < GAME_SIZE) {
+                throw new BalanceTalkException(ErrorCode.BALANCE_GAME_SIZE_TEN);
+            }
             return TempGameSet.builder()
+                    .title(title)
                     .mainTag(mainTag)
                     .subTag(subTag)
-                    .tempGames(tempGames.stream().map(CreateTempGameRequest::toEntity).toList())
                     .member(member)
                     .build();
         }
@@ -35,16 +50,35 @@ public class TempGameSetDto {
 
     @Data
     @Builder
+    @AllArgsConstructor
     @Schema(description = "임시 밸런스 게임 세트 조회 응답")
     public static class TempGameSetResponse {
 
-        private List<TempGameDetailResponse> tempGameDetailResponses;
+        @Schema(description = "밸런스게임 세트 제목", example = "밸런스게임 세트 제목")
+        private String title;
 
-        public static TempGameSetResponse fromEntity(TempGameSet tempGameSet) {
+        @Schema(description = "밸런스 게임 메인 태그", example = "커플")
+        private String mainTag;
+
+        @Schema(description = "밸런스 게임 서브 태그", example = "커플지옥")
+        private String subTag;
+
+        @Schema(description = "게임 리스트")
+        private List<TempGameResponse> tempGames;
+
+        public static TempGameSetResponse fromEntity(TempGameSet tempGameSet, FileRepository fileRepository) {
+
+            List<TempGameResponse> tempGames = tempGameSet.getTempGames().stream()
+                    .map(tempGame -> TempGameResponse.fromEntity(tempGame, fileRepository))
+                    .toList();
+
             return TempGameSetResponse.builder()
-                    .tempGameDetailResponses(tempGameSet.getTempGames().stream()
-                            .map(TempGameDetailResponse::fromEntity).toList()
-                    ).build();
+                    .title(tempGameSet.getTitle())
+                    .mainTag(tempGameSet.getMainTag().getName())
+                    .subTag(tempGameSet.getSubTag())
+                    .tempGames(tempGames)
+                    .build();
         }
+
     }
 }
