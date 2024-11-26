319ae30115a28281429fdc0346169a98888181b8
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-11-27 04:06:41 +0900

    feat: 프로필 이미지를 프렌즈 이미지로 등록 시 해당 파일이 업데이트되지 않도록 수정

--- src/main/java/balancetalk/member/application/MemberService.java
+++ src/main/java/balancetalk/member/application/MemberService.java
@@ -68,7 +68,9 @@ public class MemberService {
         if (joinRequest.hasProfileImgId()) {
             File newProfileImgFile = fileRepository.findById(joinRequest.getProfileImgId())
                     .orElseThrow(() -> new BalanceTalkException(NOT_FOUND_FILE));
-            fileHandler.relocateFile(newProfileImgFile, savedMember.getId(), FileType.MEMBER);
+            if (newProfileImgFile.isUploadedByMember()) {
+                fileHandler.relocateFile(newProfileImgFile, savedMember.getId(), FileType.MEMBER);
+            }
         }
     }
 
@@ -168,14 +170,18 @@ public class MemberService {
 
         if (memberUpdateRequest.getProfileImgId() != null) {
             if (member.hasProfileImgId()) {
-                File file = fileRepository.findById(member.getProfileImgId())
+                File oldProfileImgFile = fileRepository.findById(member.getProfileImgId())
                         .orElseThrow(() -> new BalanceTalkException(NOT_FOUND_FILE));
-                fileHandler.deleteFile(file);
+                if (oldProfileImgFile.isUploadedByMember()) {
+                    fileHandler.deleteFile(oldProfileImgFile);
+                }
             }
-            member.updateImageId(memberUpdateRequest.getProfileImgId());
-            File file = fileRepository.findById(member.getProfileImgId())
+            File newProfileImgFile = fileRepository.findById(memberUpdateRequest.getProfileImgId())
                     .orElseThrow(() -> new BalanceTalkException(NOT_FOUND_FILE));
-            fileHandler.relocateFile(file, member.getId(), FileType.MEMBER);
+            member.updateImageId(newProfileImgFile.getId());
+            if (newProfileImgFile.isUploadedByMember()) {
+                fileHandler.relocateFile(newProfileImgFile, member.getId(), FileType.MEMBER);
+            }
         }
 
         if (memberUpdateRequest.getNickname() != null) {
