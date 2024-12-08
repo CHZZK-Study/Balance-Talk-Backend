e244a23c22a487e1fd72f00acc883cb358da1215
jschoi-96 <choix712@umn.edu>
2024-12-08 12:16:50 +0900

    feat: 사용하지 않는 jpql문 삭제

--- src/main/java/balancetalk/file/domain/repository/FileRepository.java
+++ src/main/java/balancetalk/file/domain/repository/FileRepository.java
@@ -4,14 +4,10 @@ import balancetalk.file.domain.File;
 import balancetalk.file.domain.FileType;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface FileRepository extends JpaRepository<File, Long>, FileRepositoryCustom {
 
     void deleteByResourceIdAndFileType(Long tempTalkPickId, FileType fileType);
 
     void deleteByResourceIdInAndFileType(List<Long> ids, FileType fileType);
-
-    @Query("SELECT f.imgUrl from File f where f.id = :id")
-    String findImgUrlById(Long id);
 }
