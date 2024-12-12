b7de6ce26621bc4e75fe3e16fd283b87afda6cd6
jschoi-96 <choix712@umn.edu>
2024-12-13 02:22:18 +0900

    feat: 일반 회원인 경우 STANDARD로 가입

--- src/main/java/balancetalk/member/dto/MemberDto.java
+++ src/main/java/balancetalk/member/dto/MemberDto.java
@@ -1,7 +1,10 @@
 package balancetalk.member.dto;
 
+import static balancetalk.member.domain.SignupType.*;
+
 import balancetalk.member.domain.Member;
 import balancetalk.member.domain.Role;
+import balancetalk.member.domain.SignupType;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -54,12 +57,16 @@ public class MemberDto {
         @Schema(description = "회원 권한", example = "USER")
         private Role role;
 
+        @Schema(description = "회원 가입 경로", example = "STANDARD")
+        private SignupType signupType;
+
         public Member toEntity() {
             return Member.builder()
                     .nickname(nickname)
                     .email(email)
                     .password(password)
                     .role(role)
+                    .signupType(STANDARD)
                     .profileImgId(profileImgId)
                     .build();
         }
