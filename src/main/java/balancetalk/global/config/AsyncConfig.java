e0db40536a80c6888abc49346f915b8803161f3d
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-12-17 22:04:04 +0900

    chore: 비동기 처리를 위한 설정 클래스 작성

---
+++ src/main/java/balancetalk/global/config/AsyncConfig.java
@@ -0,0 +1,32 @@
+package balancetalk.global.config;
+
+import balancetalk.global.exception.CustomAsyncUncaughtExceptionHandler;
+import java.util.concurrent.Executor;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig implements AsyncConfigurer {
+
+    @Value("${async.core-pool-size}")
+    private int corePoolSize;
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(corePoolSize);
+        executor.setThreadNamePrefix("Async-");
+        executor.initialize();
+        return executor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return new CustomAsyncUncaughtExceptionHandler();
+    }
+}
